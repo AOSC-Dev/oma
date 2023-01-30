fa7331b6b86ac3287693c615eaf599ef5130d0de
eatradish <sakiiily@aosc.io>
2023-01-30 23:30:58 +0800

    blackbox: add apt -s info parse

--- src/blackbox.rs
+++ src/blackbox.rs
@@ -21,9 +21,17 @@ pub enum Action {
 pub struct AptPackage {
     name: String,
     action: AptAction,
+    now_version: Option<String>,
+    info: Option<AptPackageInfo>,
 }
 
 #[derive(Debug)]
+pub struct AptPackageInfo {
+    new_version: String,
+    branch_and_arch: String,
+}
+
+#[derive(Debug, PartialEq, Eq)]
 pub enum AptAction {
     Install,
     Remove,
@@ -47,22 +55,89 @@ pub fn apt_calc(list: &[Package]) -> Result<Vec<AptPackage>> {
     let output = std::str::from_utf8(&command.stdout)?;
 
     for i in output.lines() {
-        let action = if i.starts_with("Inst ") {
-            AptAction::Install
-        } else if i.starts_with("Remv ") {
-            AptAction::Remove
-        } else if i.starts_with("Conf ") {
-            AptAction::Configure
-        } else if i.starts_with("Purg "){
-            AptAction::Purge
-        } else {
-            continue;
+        let mut ready = i.split_whitespace();
+
+        let action = match ready.nth(0) {
+            Some("Inst") => AptAction::Install,
+            Some("Remv") => AptAction::Remove,
+            Some("Conf") => AptAction::Configure,
+            Some("Purg") => AptAction::Purge,
+            _ => continue,
         };
 
-        let mut ready = i.split_whitespace();
-        let name = ready.nth(1).take().context("invalid apt Action!")?.to_string();
+        let name = ready
+            .nth(0)
+            .take()
+            .context("invalid apt Action!")?
+            .to_string();
 
-        result.push(AptPackage { name, action })
+        let ready = ready.collect::<Vec<_>>().join(" ");
+
+        let mut ready_split = ready.split_whitespace();
+        let same_item = ready_split.nth(0);
+
+        let mut now_version = None;
+        let mut info = None;
+
+        fn info_inner(ready: String, info: &mut Option<AptPackageInfo>) -> Result<()> {
+            let info_str = ready
+                .strip_prefix("(")
+                .and_then(|x| x.strip_suffix(")"))
+                .map(|x| x.to_string());
+
+            if let Some(info_str) = info_str {
+                let mut info_str_split = info_str.split_whitespace();
+                let new_version = info_str_split
+                    .nth(0)
+                    .take()
+                    .context("Unsupported item!")?
+                    .to_string();
+                let branch_and_arch = info_str_split.collect::<Vec<_>>().join(" ").to_string();
+
+                *info = Some(AptPackageInfo {
+                    new_version,
+                    branch_and_arch,
+                })
+            }
+
+            Ok(())
+        }
+
+        if let Some(same_item) = same_item {
+            if same_item.starts_with("[") && same_item.ends_with("]") {
+                now_version = same_item
+                    .strip_prefix("[")
+                    .and_then(|x| x.strip_suffix("]"))
+                    .map(|x| x.to_string());
+                let ready = ready_split.collect::<Vec<_>>().join(" ");
+
+                if ready.starts_with("(") && ready.ends_with(")") {
+                    info_inner(ready, &mut info)?;
+                }
+            } else {
+                info_inner(ready, &mut info)?;
+            }
+        } else {
+            info_inner(ready, &mut info)?;
+        }
+        // if action == AptAction::Configure {
+        //     info = same_item_2.strip_prefix("(").and_then(|x| x.strip_suffix(")")).map(|x| x.to_string());
+        // }
+
+        // let same_item_2 = ready.nth(0);
+
+        // if let Some(same_item_2) = same_item_2 {
+        //     if same_item_2.starts_with("(") && same_item_2.ends_with(")") {
+        //         info = same_item_2.strip_prefix("(").and_then(|x| x.strip_suffix(")")).map(|x| x.to_string());
+        //     }
+        // }
+
+        result.push(AptPackage {
+            name,
+            action,
+            now_version,
+            info,
+        });
     }
 
     Ok(result)
@@ -71,7 +146,7 @@ pub fn apt_calc(list: &[Package]) -> Result<Vec<AptPackage>> {
 #[test]
 fn test() {
     let test = Package {
-        name: "go".to_string(),
+        name: "kodi".to_string(),
         action: Action::Install,
     };
 
