44264992a753feba5ab80e95aee6a5a89c861ca6
eatradish <sakiiily@aosc.io>
2023-02-03 15:16:03 +0800

    main: use clap to handle subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,56 @@
 use action::AoscptAction;
+use clap::{Parser, Subcommand};
 
+mod action;
+mod download;
+mod formatter;
 mod pkgversion;
 mod update;
 mod utils;
 mod verify;
-mod download;
-mod action;
-mod formatter;
+
+#[derive(Parser, Debug)]
+#[clap(about, version, author)]
+pub struct Args {
+    #[clap(subcommand)]
+    subcommand: AoscptCommand,
+}
+
+#[derive(Subcommand, Debug)]
+enum AoscptCommand {
+    /// Install Package
+    Install(Install),
+    /// Update Package
+    Update(Update),
+    /// Delete Package
+    Remove(Delete),
+}
+
+#[derive(Parser, Debug)]
+struct Install {
+    packages: Vec<String>,
+}
+
+#[derive(Parser, Debug)]
+struct Update {}
+
+#[derive(Parser, Debug)]
+struct Delete {
+    packages: Vec<String>,
+}
 
 fn main() {
     env_logger::init();
 
     let app = AoscptAction::new().unwrap();
 
-    if let Err(e) = app.install(&["s-tui".to_owned()]) {
-        eprintln!("{}", e);
+    let args = Args::parse();
+
+    if let Err(e) = match args.subcommand {
+        AoscptCommand::Install(v) => app.install(&v.packages),
+        AoscptCommand::Remove(v) => app.remove(&v.packages, true),
+        AoscptCommand::Update(_) => app.update(),
+    } {
+        eprintln!("{e}");
     }
 }
