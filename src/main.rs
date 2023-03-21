9f19391cf0a06bb08c2bebdbe075360e12fff62b
eatradish <sakiiily@aosc.io>
2023-03-21 17:33:31 +0800

    Revert "main: fake clap more like real clap"
    This reverts commit 2b19d1d1d43503b696d1f68e825e8db62e940851.

--- src/main.rs
+++ src/main.rs
@@ -268,7 +268,7 @@ async fn try_main() -> Result<()> {
         println!(
             "{} unexpected argument '{}' found\n",
             style("error:").red().bold(),
-            style("--ailurus").yellow().bold()
+            style("--ailurus").yellow()
         );
         println!("{}: oma <COMMAND>\n", style("Usage").bold().underlined());
         println!("For more information, try '{}'.", style("--help").bold());
