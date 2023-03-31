438b89df8796ab1b74f2c2f69ac818e5f5ae764c
eatradish <sakiiily@aosc.io>
2023-03-31 13:39:20 +0800

    feat: output man pages to /man

--- build.rs
+++ build.rs
@@ -1,16 +1,22 @@
 include!("src/args.rs");
 
 fn main() -> std::io::Result<()> {
-    let out_dir =
-        std::path::PathBuf::from(std::env::var_os("OUT_DIR").ok_or(std::io::ErrorKind::NotFound)?);
-
     let cmd = command_builder();
 
     let man = clap_mangen::Man::new(cmd);
     let mut buffer: Vec<u8> = Default::default();
     man.render(&mut buffer)?;
 
-    std::fs::write(out_dir.join("oma.1"), buffer)?;
+    let man_dir = std::path::PathBuf::from(
+        std::env::var_os("CARGO_MANIFEST_DIR").ok_or(std::io::ErrorKind::NotFound)?,
+    )
+    .join("man");
+
+    if !man_dir.is_dir() {
+        std::fs::create_dir_all(&man_dir)?;
+    }
+
+    std::fs::write(man_dir.join("oma.1"), buffer)?;
 
     Ok(())
 }
