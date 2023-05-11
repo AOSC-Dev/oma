6f7941995a45ed1b1efdaeb6b7e6082bb0feccc2
eatradish <sakiiily@aosc.io>
2023-05-11 19:10:28 +0800

    fix: use right method to get apt style source

--- src/utils.rs
+++ src/utils.rs
@@ -179,12 +179,12 @@ pub fn error_due_to<
 // input: like http://50.50.1.183/debs/pool/stable/main/f/fish_3.6.0-0_amd64.deb
 // output: http://50.50.1.183/debs stable main
 pub fn source_url_to_apt_style(s: &str) -> Option<String> {
-    let mut s_split = s.split('/').rev();
-    let component = s_split.nth(2)?;
-    let branch = s_split.next()?;
-    let pool = s_split.position(|x| x == "pool")?;
+    let mut s_split = s.split('/');
+    let component = s_split.nth_back(2)?;
+    let branch = s_split.next_back()?;
+    let _ = s_split.next_back();
 
-    let host = &s_split.rev().collect::<Vec<_>>().join("/")[pool..];
+    let host = &s_split.collect::<Vec<_>>().join("/");
 
     Some(format!("{host} {branch} {component}"))
 }
