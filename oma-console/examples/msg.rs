f310ff2486eaba37b8e659991429a81dfea4dff7
eatradish <sakiiily@aosc.io>
2023-06-19 05:35:04 +0800

    refactor(oma-console): do not const Writer::default as WRITER

--- oma-console/examples/msg.rs
+++ oma-console/examples/msg.rs
@@ -1,11 +1,12 @@
-use oma_console::{info, writer::WRITER, msg, debug, warn, error, due_to, success};
+use oma_console::{debug, due_to, error, info, msg, success, warn, writer::Writer};
 
 fn main() {
-    msg!(WRITER, "Welcome");
-    debug!(WRITER, "Hello");
-    info!(WRITER, "I'am fine");
-    warn!(WRITER, "Thank you");
-    error!(WRITER, "and you?");
-    due_to!(WRITER, "QAQ");
-    success!(WRITER, "PAP");
+    let writer = Writer::default();
+    msg!(writer, "Welcome");
+    debug!(writer, "Hello");
+    info!(writer, "I'am fine");
+    warn!(writer, "Thank you");
+    error!(writer, "and you?");
+    due_to!(writer, "QAQ");
+    success!(writer, "PAP");
 }
