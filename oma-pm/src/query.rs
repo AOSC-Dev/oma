f440560c51edffa6a78a8e22af14ce3177c54f97
eatradish <sakiiily@aosc.io>
2023-07-04 22:08:26 +0800

    feat(oma-pm): add query_from_version and query_from_branch function

--- oma-pm/src/query.rs
+++ oma-pm/src/query.rs
@@ -9,6 +9,12 @@ use crate::pkginfo::PkgInfo;
 pub enum OmaDatabaseError {
     #[error(transparent)]
     RustApt(#[from] rust_apt::util::Exception),
+    #[error("Invaild pattern: {0}")]
+    InvaildPattern(String),
+    #[error("Can not find package {0} from database")]
+    NoPackage(String),
+    #[error("Pkg {0} has no version {1}")]
+    NoVersion(String, String),
 }
 
 pub struct OmaDatabase {
@@ -24,7 +30,7 @@ impl OmaDatabase {
         })
     }
 
-    pub fn search_from_glob(
+    pub fn query_from_glob(
         &self,
         glob: &str,
         filter_candidate: bool,
@@ -50,13 +56,36 @@ impl OmaDatabase {
 
         Ok(res)
     }
+
+    pub fn query_from_version(&self, pat: &str) -> OmaDatabaseResult<PkgInfo> {
+        let (pkgname, version) = pat
+            .split_once('=')
+            .ok_or_else(|| OmaDatabaseError::InvaildPattern(pat.to_string()))?;
+
+        let pkg = self
+            .cache
+            .get(pkgname)
+            .ok_or_else(|| OmaDatabaseError::NoPackage(pkgname.to_string()))?;
+
+        let version = pkg
+            .get_version(version)
+            .ok_or_else(|| OmaDatabaseError::NoVersion(pkgname.to_string(), version.to_string()))?.unique();
+
+        let info = PkgInfo::new(&self.cache, version, &pkg);
+
+        Ok(info)
+    }
+
+    pub fn query_from_branch(&self, pat: &str) -> OmaDatabaseResult<PkgInfo> {
+        todo!()
+    }
 }
 
 #[test]
 fn test_glob_search() {
     let db = OmaDatabase::new().unwrap();
-    let res_filter = db.search_from_glob("apt*", true).unwrap();
-    let res = db.search_from_glob("apt*", false).unwrap();
+    let res_filter = db.query_from_glob("apt*", true).unwrap();
+    let res = db.query_from_glob("apt*", false).unwrap();
 
     for i in res_filter {
         println!("{}", i);
