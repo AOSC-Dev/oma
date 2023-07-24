7874c036cfc8ab1de0d909ded18dee5d686b248b
eatradish <sakiiily@aosc.io>
2023-07-24 14:52:24 +0800

    test(oma-pm): add download pkgs example

---
+++ oma-pm/examples/download_pkgs.rs
@@ -0,0 +1,21 @@
+use std::path::Path;
+
+use oma_pm::apt::{OmaApt, OmaAptError};
+use tracing_subscriber::{EnvFilter, fmt, prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt};
+
+fn main() -> Result<(), OmaAptError> {
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
+
+    let apt = OmaApt::new()?;
+
+    let pkgs = apt.select_pkg(vec!["texlive", "go"])?;
+    std::fs::create_dir_all("./test").unwrap();
+    let res = apt.download(pkgs, None, Some(Path::new("test")))?;
+
+    dbg!(res);
+
+    Ok(())
+}
