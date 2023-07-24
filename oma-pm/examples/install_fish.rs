e8e83b6dfcfd669a62bc54f2a941a9932bbffcf5
eatradish <sakiiily@aosc.io>
2023-07-24 13:46:25 +0800

    test(oma-pm): refactor

--- oma-pm/examples/install_fish.rs
+++ oma-pm/examples/install_fish.rs
@@ -1,10 +1,8 @@
 use oma_pm::apt::{OmaAptError, OmaApt, select_pkg, AptArgs};
-use rust_apt::new_cache;
 
 fn main() -> Result<(), OmaAptError> {
     let apt = OmaApt::new()?;
-    let cache = new_cache!()?;
-    let pkgs = select_pkg(vec!["fish"], &cache)?;
+    let pkgs = select_pkg(vec!["fish"], &apt.cache)?;
 
     apt.install(pkgs, false)?;
     let op = apt.operation_vec()?;
