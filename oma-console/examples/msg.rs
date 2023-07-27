b8fc1a95ccb112e3f0be406f3ab7c6b70fcfefef
eatradish <sakiiily@aosc.io>
2023-07-27 17:26:09 +0800

    refactor(oma-console): improve debug marco

--- oma-console/examples/msg.rs
+++ oma-console/examples/msg.rs
@@ -1,12 +1,11 @@
-use oma_console::{debug, due_to, error, info, msg, success, warn, writer::Writer};
+use oma_console::{debug, due_to, error, info, msg, success, warn};
 
 fn main() {
-    let writer = Writer::default();
-    msg!(writer, "Welcome");
-    debug!(writer, "Hello");
-    info!(writer, "I'am fine");
-    warn!(writer, "Thank you");
-    error!(writer, "and you?");
-    due_to!(writer, "QAQ");
-    success!(writer, "PAP");
+    msg!("Welcome");
+    debug!("Hello");
+    info!("I'am fine");
+    warn!("Thank you");
+    error!("and you?");
+    due_to!("QAQ");
+    success!("PAP");
 }
--- oma-console/src/lib.rs
+++ oma-console/src/lib.rs
@@ -2,10 +2,13 @@ pub mod pager;
 pub mod pb;
 pub mod writer;
 pub use console;
-pub use indicatif;
 pub use dialoguer;
+pub use indicatif;
+use once_cell::sync::Lazy;
+use writer::Writer;
 
 pub type Result<T> = std::result::Result<T, OmaConsoleError>;
+pub static WRITER: Lazy<Writer> = Lazy::new(writer::Writer::default);
 
 #[derive(Debug, thiserror::Error)]
 pub enum OmaConsoleError {
@@ -18,49 +21,56 @@ pub enum OmaConsoleError {
 // We will ignore write errors in the following macros, since cannot print messages is not an emergency
 #[macro_export]
 macro_rules! msg {
-    ($write:ident, $($arg:tt)+) => {
-        $write.writeln("", &format!($($arg)+), false).ok();
+    ($($arg:tt)+) => {
+        use oma_console::WRITER as MSG_WRITER;
+        MSG_WRITER.writeln("", &format!($($arg)+), false).ok();
     };
 }
 
 #[macro_export]
 macro_rules! debug {
-    ($write:ident, $($arg:tt)+) => {
-        $write.writeln(&console::style("DEBUG").dim().to_string(), &format!($($arg)+), false).ok();
+    ($($arg:tt)+) => {
+        use oma_console::WRITER as DEBUG_WRITER;
+        DEBUG_WRITER.writeln(&console::style("DEBUG").dim().to_string(), &format!($($arg)+), false).ok();
     };
 }
 
 #[macro_export]
 macro_rules! success {
-    ($write:ident, $($arg:tt)+) => {
-        $write.writeln(&console::style("SUCCESS").green().bold().to_string(), &format!($($arg)+), false).ok();
+    ($($arg:tt)+) => {
+        use oma_console::WRITER as SUCCESS_WRITER;
+        SUCCESS_WRITER.writeln(&console::style("SUCCESS").green().bold().to_string(), &format!($($arg)+), false).ok();
     };
 }
 
 #[macro_export]
 macro_rules! info {
-    ($write:ident, $($arg:tt)+) => {
-        $write.writeln(&console::style("INFO").blue().bold().to_string(), &format!($($arg)+), false).ok();
+    ($($arg:tt)+) => {
+        use oma_console::WRITER as INFO_WRITER;
+        INFO_WRITER.writeln(&console::style("INFO").blue().bold().to_string(), &format!($($arg)+), false).ok();
     };
 }
 
 #[macro_export]
 macro_rules! warn {
-    ($write:ident, $($arg:tt)+) => {
-        $write.writeln(&console::style("WARNING").yellow().bold().to_string(), &format!($($arg)+), false).ok();
+    ($($arg:tt)+) => {
+        use oma_console::WRITER as WARN_WRITER;
+        WARN_WRITER.writeln(&console::style("WARNING").yellow().bold().to_string(), &format!($($arg)+), false).ok();
     };
 }
 
 #[macro_export]
 macro_rules! error {
-    ($write:ident, $($arg:tt)+) => {
-        $write.writeln(&console::style("ERROR").red().bold().to_string(), &format!($($arg)+), false).ok();
+    ($($arg:tt)+) => {
+        use oma_console::WRITER as ERR_WRITER;
+        ERR_WRITER.writeln(&console::style("ERROR").red().bold().to_string(), &format!($($arg)+), false).ok();
     };
 }
 
 #[macro_export]
 macro_rules! due_to {
-    ($write:ident, $($arg:tt)+) => {
-        $write.writeln(&console::style("DUE TO").yellow().bold().to_string(), &format!($($arg)+), false).ok();
+    ($($arg:tt)+) => {
+        use oma_console::WRITER as DUE_TO_WRITER;
+        DUE_TO_WRITER.writeln(&console::style("DUE TO").yellow().bold().to_string(), &format!($($arg)+), false).ok();
     };
 }
