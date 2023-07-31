898a17812a0a9d7cfe72768a491e7d0999917764
eatradish <sakiiily@aosc.io>
2023-07-31 10:22:58 +0800

    refactor(oma-contents): redesign api

--- oma-contents/examples/list_files.rs
+++ oma-contents/examples/list_files.rs
@@ -1,37 +1,23 @@
 use std::{path::Path, time::Duration};
 
 use indicatif::ProgressBar;
-use oma_contents::{find, Event, Request};
+use oma_contents::{find, QueryMode};
+use oma_utils::dpkg_arch;
 
 fn main() {
-    let (tx, rx) = std::sync::mpsc::channel();
-    let req = Request {
-        kw: "apt",
-        is_list: true,
-        cnf: false,
-        only_bin: false,
-        dists_dir: Path::new("/var/lib/apt/lists"),
-        arch: "amd64",
-        event: Some(tx),
-    };
-
     let pb = ProgressBar::new_spinner();
     pb.enable_steady_tick(Duration::from_millis(100));
+    pb.set_message("Searching ...");
 
-    let r = std::thread::spawn(move || find(req));
+    let r = find(
+        "apt",
+        QueryMode::ListFiles,
+        Path::new("/var/lib/apt/lists"),
+        &dpkg_arch().unwrap(),
+        |c| {
+            pb.set_message(format!("Searching, found {c} results so far ..."));
+        },
+    );
 
-    while let Ok(log) = rx.recv() {
-        match log {
-            Event::Info(s) => pb.set_message(format!("INFO {s}")),
-            Event::Warn(s) => pb.set_message(format!("WARN {s}")),
-            Event::Error(s) => pb.set_message(format!("ERROR {s}")),
-            Event::Searching => pb.set_message("Searching ..."),
-            Event::SearchingWithResult(index) => {
-                pb.set_message(format!("Searching, found {index} results so far ..."))
-            }
-            Event::Done => pb.set_message("DONE."),
-        }
-    }
-
-    println!("{:?}", r.join().unwrap());
+    dbg!(r.unwrap());
 }
