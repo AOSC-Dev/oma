f0db5b3bfd3687d0f7b994cdcd257384b866a145
eatradish <sakiiily@aosc.io>
2023-08-01 18:15:35 +0800

    test(oma-pm): update example

--- oma-pm/examples/install_fish.rs
+++ oma-pm/examples/install_fish.rs
@@ -5,7 +5,7 @@ fn main() -> Result<(), OmaAptError> {
     let apt = OmaApt::new(vec![], oma_apt_args)?;
     let pkgs = apt.select_pkg(vec!["fish"], false, true)?;
 
-    apt.install(pkgs, false, true, false)?;
+    apt.install(pkgs, false)?;
     // let op = apt.operation_vec()?;
 
     apt.commit(None, &AptArgs::default(), false)?;
