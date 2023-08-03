043ca948b7553c1e231bb92e08838717c0445423
eatradish <sakiiily@aosc.io>
2023-08-03 12:21:19 +0800

    refactor: oma depends is back

--- src/command.rs
+++ src/command.rs
@@ -340,6 +340,38 @@ pub fn find(x: &str, is_bin: bool, pkg: String) -> Result<i32> {
     Ok(0)
 }
 
+pub fn depends(pkgs: Vec<String>) -> Result<i32> {
+    let oma_apt_args = OmaAptArgsBuilder::default().build()?;
+    let apt = OmaApt::new(vec![], oma_apt_args)?;
+
+    let pkgs = apt.select_pkg(
+        pkgs.iter().map(|x| x.as_str()).collect::<Vec<_>>(),
+        false,
+        true,
+    )?;
+
+    for pkg in pkgs {
+        println!("{}:", pkg.raw_pkg.name());
+        let all_deps = pkg.deps;
+
+        for (k, v) in all_deps {
+            for dep in v.inner() {
+                for b_dep in dep {
+                    let s = if let Some(comp_ver) = b_dep.comp_ver {
+                        Cow::Owned(format!("({comp_ver})"))
+                    } else {
+                        Cow::Borrowed("")
+                    };
+
+                    println!("  {k}: {} {}", b_dep.name, s);
+                }
+            }
+        }
+    }
+
+    Ok(0)
+}
+
 pub fn pick(pkg_str: String, no_refresh: bool) -> Result<i32> {
     if !no_refresh {
         refresh()?;
