b097de9165dc0f1a8d970b750c84d6f5fc8ead81
eatradish <sakiiily@aosc.io>
2023-08-07 14:32:25 +0800

    refactor(oma-fetch): use builder api design

--- oma-fetch/examples/downloads.rs
+++ oma-fetch/examples/downloads.rs
@@ -1,6 +1,8 @@
 use std::path::PathBuf;
 
-use oma_fetch::{DownloadEntry, DownloadResult, DownloadSource, DownloadSourceType, OmaFetcher};
+use oma_fetch::{
+    DownloadEntryBuilder, DownloadResult, DownloadSource, DownloadSourceType, OmaFetcher,
+};
 use tokio::io::AsyncWriteExt;
 
 #[tokio::main]
@@ -10,42 +12,36 @@ async fn main() -> DownloadResult<()> {
         DownloadSourceType::Http
     );
 
-    let file_1 = DownloadEntry::new(
-        vec![source_1],
-        "go.deb".to_string(),
-        PathBuf::from("./oma-fetcher-test"),
-        Some("0625cbba48a14438eea144682567a026a17e173420c5bdcbc06dcb11aba50628".to_string()),
-        true,
-        None,
-    );
+    let file_1 = DownloadEntryBuilder::default()
+        .source(vec![source_1])
+        .filename("go.deb".to_string())
+        .dir(PathBuf::from("./oma-fetcher-test"))
+        .hash("0625cbba48a14438eea144682567a026a17e173420c5bdcbc06dcb11aba50628".to_string())
+        .allow_resume(true)
+        .build()?;
 
     let source_2 = DownloadSource::new(
         "https://mirrors.bfsu.edu.cn/anthon/debs/pool/stable/main/v/vscodium_1.77.3.23102-0_arm64.deb".to_string(),
         DownloadSourceType::Http
     );
 
-    let file_2 = DownloadEntry::new(
-        vec![source_2],
-        "vscode.deb".to_string(),
-        PathBuf::from("./oma-fetcher-test"),
-        None,
-        true,
-        None,
-    );
+    let file_2 = DownloadEntryBuilder::default()
+        .source(vec![source_2])
+        .filename("vscode.deb".to_string())
+        .dir(PathBuf::from("./oma-fetcher-test"))
+        .allow_resume(false)
+        .build()?;
 
     let mut test_local_file = tokio::fs::File::create("test").await?;
     test_local_file.write_all(b"test").await?;
 
     let source_3 = DownloadSource::new("./test".to_string(), DownloadSourceType::Local);
 
-    let file_3 = DownloadEntry::new(
-        vec![source_3],
-        "test_downloaded".to_string(),
-        PathBuf::from("./oma-fetcher-test"),
-        None,
-        false,
-        None,
-    );
+    let file_3 = DownloadEntryBuilder::default()
+        .source(vec![source_3])
+        .filename("test_downloaded".to_string())
+        .dir(PathBuf::from("./oma-fetcher-test"))
+        .build()?;
 
     let fetcher = OmaFetcher::new(None, true, None, vec![file_1, file_2, file_3], None)?;
 
