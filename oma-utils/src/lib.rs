ec61dda03a3ad18f3b9b34db398b39c550e0abbf
eatradish <sakiiily@aosc.io>
2023-08-22 17:07:35 +0800

    feat(oma-utils): use feature to select abstract code

--- oma-utils/src/lib.rs
+++ oma-utils/src/lib.rs
@@ -1,6 +1,10 @@
 pub use os_release::OsRelease;
 
+#[cfg(feature = "dbus")]
 pub mod dbus;
+#[cfg(feature = "dpkg")]
 pub mod dpkg;
+#[cfg(feature = "human-bytes")]
 pub mod human_bytes;
+#[cfg(feature = "oma")]
 pub mod oma;
