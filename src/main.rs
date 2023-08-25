128c85cf11311743f76944d05cc481db268a11d4
eatradish <sakiiily@aosc.io>
2023-08-25 12:52:30 +0800

    feat(oma-pm): use chrono to get and parse datetime

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,6 @@ use oma_console::{console::style, info};
 use oma_console::{debug, due_to, error, DEBUG, WRITER};
 use oma_utils::oma::{terminal_ring, unlock_oma};
 use oma_utils::OsRelease;
-use once_cell::sync::Lazy;
-use time::{UtcOffset, OffsetDateTime};
-use time::macros::offset;
 
 use std::sync::atomic::{AtomicBool, Ordering};
 
@@ -29,8 +26,6 @@ use oma_console::pager::SUBPROCESS;
 static ALLOWCTRLC: AtomicBool = AtomicBool::new(false);
 static LOCKED: AtomicBool = AtomicBool::new(false);
 static AILURUS: AtomicBool = AtomicBool::new(false);
-static TIME_OFFSET: Lazy<UtcOffset> =
-    Lazy::new(|| UtcOffset::local_offset_at(OffsetDateTime::UNIX_EPOCH).unwrap_or(offset!(UTC)));
 
 #[derive(Debug, Default)]
 pub struct InstallArgs {
@@ -65,9 +60,6 @@ pub struct RemoveArgs {
 }
 
 fn main() {
-    // FIXME: 多线程环境下无法获取 time offset
-    let _ = &*TIME_OFFSET;
-
     ctrlc::set_handler(single_handler).expect(
         "Oma could not initialize SIGINT handler.\n\nPlease restart your installation environment.",
     );
