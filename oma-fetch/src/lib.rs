0eeda08accadd2b2e0e9574bb680a4a3b42ad64c
eatradish <sakiiily@aosc.io>
2023-09-04 14:43:59 +0800

    feat(oma-fetch): switch to callback event, no more indicatif in oma-fetch

--- oma-fetch/src/lib.rs
+++ oma-fetch/src/lib.rs
@@ -1,17 +1,15 @@
-use std::{path::PathBuf, sync::Arc};
+use std::{
+    path::PathBuf,
+    sync::{atomic::AtomicU64, Arc},
+};
 
 use derive_builder::Builder;
 use futures::StreamExt;
-use oma_console::{
-    indicatif::{self, MultiProgress, ProgressBar},
-    writer::Writer,
-};
 
 use reqwest::{Client, ClientBuilder};
 
 pub mod checksum;
 mod download;
-mod lang;
 
 use download::try_download;
 
@@ -27,8 +25,6 @@ pub enum DownloadError {
     ReqwestError(#[from] reqwest::Error),
     #[error(transparent)]
     ChecksumError(#[from] crate::checksum::ChecksumError),
-    #[error(transparent)]
-    TemplateError(#[from] indicatif::style::TemplateError),
     #[error("Failed to open local source file {0}: {1}")]
     FailedOpenLocalSourceFile(String, String),
     #[error("Download all file failed: {0}: {1}")]
@@ -94,36 +90,12 @@ impl Ord for DownloadSourceType {
     }
 }
 
-#[derive(Clone)]
-pub struct FetchProgressBar {
-    pub mb: Arc<MultiProgress>,
-    pub global_bar: Option<ProgressBar>,
-    pub progress: Option<(usize, usize)>,
-    pub msg: Option<String>,
-}
-
-impl FetchProgressBar {
-    pub fn new(
-        mb: Arc<MultiProgress>,
-        global_bar: Option<ProgressBar>,
-        progress: Option<(usize, usize)>,
-        msg: Option<String>,
-    ) -> Self {
-        Self {
-            mb,
-            global_bar,
-            progress,
-            msg,
-        }
-    }
-}
-
 pub struct OmaFetcher {
     client: Client,
-    bar: Option<(Arc<MultiProgress>, Option<ProgressBar>)>,
     download_list: Vec<DownloadEntry>,
     limit_thread: usize,
     retry_times: usize,
+    global_progress: Arc<AtomicU64>,
 }
 
 #[derive(Debug)]
@@ -132,6 +104,18 @@ pub struct Summary {
     pub writed: bool,
     pub count: usize,
     pub context: Option<String>,
+}
+
+#[derive(Debug)]
+pub enum DownloadEvent {
+    ChecksumMismatchRetry { filename: String, times: usize },
+    GlobalProgressSet(u64),
+    GlobalProgressInc(u64),
+    ProgressDone,
+    NewProgressSpinner(String),
+    NewProgress(u64, String),
+    ProgressInc(u64),
+    CanNotGetSourceNextUrl(String),
 }
 
 /// Summary struct to save download result
@@ -150,43 +134,17 @@ impl Summary {
 impl OmaFetcher {
     pub fn new(
         client: Option<Client>,
-        bar: bool,
-        total_size: Option<u64>,
         download_list: Vec<DownloadEntry>,
         limit_thread: Option<usize>,
     ) -> DownloadResult<Self> {
         let client = client.unwrap_or(ClientBuilder::new().user_agent("oma").build()?);
 
-        let bar = if bar {
-            let mb = Arc::new(MultiProgress::new());
-            let writer = Writer::default();
-            let gpb = if let Some(total_size) = total_size {
-                Some(
-                    mb.insert(
-                        0,
-                        ProgressBar::new(total_size)
-                            .with_style(oma_console::pb::oma_style_pb(writer, true)?),
-                    ),
-                )
-            } else {
-                None
-            };
-
-            if let Some(ref gpb) = gpb {
-                gpb.set_message("Progress")
-            }
-
-            Some((mb, gpb))
-        } else {
-            None
-        };
-
         Ok(Self {
             client,
-            bar,
             download_list,
             limit_thread: limit_thread.unwrap_or(4),
             retry_times: 3,
+            global_progress: Arc::new(AtomicU64::new(0)),
         })
     }
 
@@ -197,37 +155,27 @@ impl OmaFetcher {
     }
 
     /// Start download
-    pub async fn start_download(&self) -> Vec<DownloadResult<Summary>> {
+    pub async fn start_download<F>(&self, callback: F) -> Vec<DownloadResult<Summary>>
+    where
+        F: Fn(usize, DownloadEvent) + Clone + Send + Sync,
+    {
         let mut tasks = Vec::new();
         for (i, c) in self.download_list.iter().enumerate() {
-            let fpb = if let Some((mb, gpb)) = &self.bar {
-                Some(FetchProgressBar {
-                    mb: mb.clone(),
-                    global_bar: gpb.clone(),
-                    progress: Some((i + 1, self.download_list.len())),
-                    msg: c.msg.clone(),
-                })
-            } else {
-                None
-            };
-
             tasks.push(try_download(
                 &self.client,
                 c,
-                fpb,
+                (i + 1, self.download_list.len(), c.msg.clone()),
                 i,
                 self.retry_times,
                 c.msg.clone(),
+                callback.clone(),
+                self.global_progress.clone(),
             ));
         }
 
         let stream = futures::stream::iter(tasks).buffer_unordered(self.limit_thread);
 
         let res = stream.collect::<Vec<_>>().await;
-
-        if let Some(gpb) = self.bar.as_ref().and_then(|x| x.1.as_ref()) {
-            gpb.finish_and_clear();
-        }
 
         res
     }
