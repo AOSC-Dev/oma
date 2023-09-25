c82a5f4e7b048a6ee31df9f5c92919933e4dcf23
eatradish <sakiiily@aosc.io>
2023-09-25 18:42:42 +0800

    feat(oma-contents): No more fallback to grep crate when no ripgrep binary is available
    - Refactored the way oma searches when it doesn't use ripgrep to support searching for compressed Contents files.
      (you need to use `--no-default-feature --features contents-without-rg` to turn it on)

--- oma-contents/src/lib.rs
+++ oma-contents/src/lib.rs
@@ -1,18 +1,13 @@
 use std::{
-    io::{BufRead, BufReader},
-    path::Path,
-    process::{Command, Stdio},
+    io::BufReader,
+    path::{Path, PathBuf},
     sync::{Arc, Mutex},
 };
 
 use chrono::{DateTime, Utc};
-use grep::{
-    regex::RegexMatcherBuilder,
-    searcher::{sinks::UTF8, Searcher},
-};
 
 use winnow::{
-    combinator::{opt, separated0, separated_pair},
+    combinator::{separated0, separated_pair},
     error::ParserError,
     token::{tag, take_till0, take_until1},
     PResult, Parser,
@@ -24,6 +19,11 @@ type Result<T> = std::result::Result<T, OmaContentsError>;
 pub enum OmaContentsError {
     #[error("Contents does not exist")]
     ContentsNotExist,
+    #[error(transparent)]
+    WhichError(#[from] which::Error),
+    #[cfg(feature = "no-rg-binary")]
+    #[error(transparent)]
+    LzzzErr(#[from] lzzzz::lz4f::Error),
     #[error("Execute ripgrep failed: {0}")]
     ExecuteRgFailed(String),
     #[error(transparent)]
@@ -74,24 +74,6 @@ pub fn find<F>(
 where
     F: Fn(ContentsEvent) + Send + Sync + Clone + 'static,
 {
-    let callback = Arc::new(callback);
-    let kw = if Path::new(keyword).is_absolute() {
-        // 当确定 keyword 是一个绝对路径时，则 strip / 肯定有值，所以直接 unwrap
-        // 这在 Windows 上可能会崩溃，后面 oma 要跑在 Windows 上面再说吧……
-        keyword.strip_prefix('/').unwrap()
-    } else {
-        keyword
-    };
-
-    let kw_escape = regex::escape(kw);
-
-    let pattern = match query_mode {
-        QueryMode::Provides(_) | QueryMode::CommandNotFound => {
-            format!(r"^(.*?{kw_escape}(?:.*[^\s])?)\s+(\S+)\s*$")
-        }
-        QueryMode::ListFiles(_) => format!(r"^\s*(.*?)\s+((?:\S*[,/])?{kw_escape}(?:,\S*|))\s*$"),
-    };
-
     let dir = std::fs::read_dir(dist_dir)?;
     let paths = Arc::new(Mutex::new(Vec::new()));
     for i in dir.flatten() {
@@ -133,7 +115,7 @@ where
                 .map(|x| x.contains("_Contents-all"))
                 .unwrap_or(false)
         {
-            paths.push(i.path());
+            paths.lock().unwrap().push(i.path());
         }
     }
 
@@ -162,129 +144,183 @@ where
         Ok(())
     });
 
-    // 如果安装了 ripgrep，则使用 rg 来进行搜索操作，因为 rg 的速度比 grep 快十倍
-    let mut res = if which::which("rg").is_ok() {
-        let mut res = vec![];
-
-        let mut cmd = Command::new("rg");
-        cmd.arg("-N"); // 不要输出行号
-        cmd.arg("-I"); // 不要输出文件名
-        cmd.arg("-e");
-        cmd.arg(pattern);
-        cmd.args(&*paths.lock().unwrap());
-
-        if search_zip {
-            cmd.arg("--search-zip");
-        }
-
-        cmd.stdout(Stdio::piped());
-
-        let mut cmd = cmd
-            .spawn()
-            .map_err(|e| OmaContentsError::ExecuteRgFailed(e.to_string()))?;
-
-        {
-            let stdout = cmd
-                .stdout
-                .as_mut()
-                .expect("Unexpected error: can not get stdout, maybe you environment is broken?");
-
-            let stdout_reader = BufReader::new(stdout);
-            let stdout_lines = stdout_reader.lines();
-
-            let mut count = 0;
-
-            let mut has_res = false;
-
-            for i in stdout_lines.flatten() {
-                if let Some(line) =
-                    parse_line(&i, matches!(query_mode, QueryMode::ListFiles(_)), kw)
-                {
-                    count += 1;
-                    callback(ContentsEvent::Progress(count));
-                    if query_mode == QueryMode::CommandNotFound {
-                        if !cfg!(feature = "aosc")
-                            && !line.1.contains("/bin")
-                            && !line.1.contains("/sbin")
-                        {
-                            continue;
-                        }
-
-                        let path_filename = line.1.split('/').last().ok_or_else(|| {
-                            OmaContentsError::ContentsEntryMissingPathList(line.1.to_string())
-                        })?;
-
-                        if strsim::jaro_winkler(kw, path_filename).abs() < 0.9 {
-                            continue;
-                        }
-                    }
-                    if !res.contains(&line) {
-                        res.push(line);
-                    }
-                    has_res = true;
-                }
-            }
-
-            if !has_res {
-                return Err(OmaContentsError::NoResult);
-            }
-
-            callback(ContentsEvent::Done);
-        }
-
-        if !cmd.wait()?.success() {
-            return Err(OmaContentsError::RgWithError);
-        }
-
-        res
-    } else {
-        // 如果没有 rg，则 fallback 到使用 grep 库，缺点是比较慢
-        let mut matcher = RegexMatcherBuilder::new();
-
-        matcher
-            .case_smart(true)
-            .case_insensitive(false)
-            .multi_line(false)
-            .unicode(false)
-            .octal(false)
-            .word(false);
-
-        let matcher = matcher.build(&pattern)?;
-        let mut searcher = Searcher::new();
-        let mut res = Vec::new();
-
-        for i in &*paths.lock().unwrap() {
-            searcher.search_path(
-                matcher.clone(),
-                i,
-                UTF8(|_, line| {
-                    let line = parse_line(line, matches!(query_mode, QueryMode::ListFiles(_)), kw);
-                    if let Some(l) = line {
-                        if query_mode == QueryMode::CommandNotFound
-                            && l.1.split_whitespace().last() != Some(kw)
-                        {
-                            return Ok(true);
-                        }
-                        if !res.contains(&l) {
-                            res.push(l);
-                        }
-                    }
-
-                    Ok(true)
-                }),
-            )?;
-        }
-
-        res
-    };
-
+    let mut res = search(paths, search_zip, query_mode, keyword, callback)?;
     res.sort_unstable();
 
     Ok(res)
 }
 
+#[cfg(not(feature = "no-rg-binary"))]
+fn search<F>(
+    paths: Arc<Mutex<Vec<PathBuf>>>,
+    search_zip: bool,
+    query_mode: QueryMode,
+    kw: &str,
+    callback: F,
+) -> Result<Vec<(String, String)>>
+where
+    F: Fn(ContentsEvent) + Send + Sync + Clone + 'static,
+{
+    use std::io::BufRead;
+    use std::process::{Command, Stdio};
+    let kw = if Path::new(kw).is_absolute() {
+        // 当确定 keyword 是一个绝对路径时，则 strip / 肯定有值，所以直接 unwrap
+        // 这在 Windows 上可能会崩溃，后面 oma 要跑在 Windows 上面再说吧……
+        kw.strip_prefix('/').unwrap()
+    } else {
+        kw
+    };
+
+    let kw_escape = regex::escape(kw);
+
+    let pattern = match query_mode {
+        QueryMode::Provides(_) | QueryMode::CommandNotFound => {
+            format!(r"^(.*?{kw_escape}(?:.*[^\s])?)\s+(\S+)\s*$")
+        }
+        QueryMode::ListFiles(_) => format!(r"^\s*(.*?)\s+((?:\S*[,/])?{kw_escape}(?:,\S*|))\s*$"),
+    };
+
+    let mut res = vec![];
+    which::which("rg")?;
+    let mut cmd = Command::new("rg");
+    cmd.arg("-N");
+    cmd.arg("-I");
+    cmd.arg("-e");
+    cmd.arg(pattern);
+    cmd.args(&*paths.lock().unwrap());
+    if search_zip {
+        cmd.arg("--search-zip");
+    }
+    cmd.stdout(Stdio::piped());
+
+    let mut cmd = cmd
+        .spawn()
+        .map_err(|e| OmaContentsError::ExecuteRgFailed(e.to_string()))?;
+    {
+        let stdout = cmd
+            .stdout
+            .as_mut()
+            .expect("Unexpected error: can not get stdout, maybe you environment is broken?");
+
+        let stdout_reader = BufReader::new(stdout);
+        let stdout_lines = stdout_reader.lines();
+
+        let mut count = 0;
+
+        let mut has_res = false;
+
+        for i in stdout_lines.flatten() {
+            if let Some(line) = search_line(&i, matches!(query_mode, QueryMode::ListFiles(_)), kw) {
+                count += 1;
+                callback(ContentsEvent::Progress(count));
+                if query_mode == QueryMode::CommandNotFound {
+                    if !cfg!(feature = "aosc")
+                        && !line.1.contains("/bin")
+                        && !line.1.contains("/sbin")
+                    {
+                        continue;
+                    }
+
+                    let path_filename = line.1.split('/').last().ok_or_else(|| {
+                        OmaContentsError::ContentsEntryMissingPathList(line.1.to_string())
+                    })?;
+
+                    if strsim::jaro_winkler(kw, path_filename).abs() < 0.9 {
+                        continue;
+                    }
+                }
+                if !res.contains(&line) {
+                    res.push(line);
+                }
+                has_res = true;
+            }
+        }
+
+        if !has_res {
+            return Err(OmaContentsError::NoResult);
+        }
+
+        callback(ContentsEvent::Done);
+    }
+    if !cmd.wait()?.success() {
+        return Err(OmaContentsError::RgWithError);
+    }
+    Ok(res)
+}
+
+#[cfg(feature = "no-rg-binary")]
+fn search<F>(
+    paths: Arc<Mutex<Vec<PathBuf>>>,
+    search_zip: bool,
+    query_mode: QueryMode,
+    kw: &str,
+    callback: F,
+) -> Result<Vec<(String, String)>>
+where
+    F: Fn(ContentsEvent) + Send + Sync + Clone + 'static,
+{
+    use rayon::prelude::*;
+    let paths = &*paths.lock().unwrap();
+
+    let callback = Arc::new(callback);
+
+    let contents = paths
+        .par_iter()
+        .filter_map(move |path| read_contents(path, search_zip).ok())
+        .collect::<Vec<_>>();
+
+    let is_list = matches!(query_mode, QueryMode::ListFiles(_));
+    let mut res = vec![];
+    let mut count = 0;
+
+    for i in contents {
+        Some(()).and_then(|_| {
+            let contents_entry = multi_line::<()>(&mut i.as_str()).ok()?;
+
+            for i in contents_entry {
+                for (_, pkg) in i.1 {
+                    if (is_list && pkg == kw) || (!is_list && i.0.contains(kw)) {
+                        count += 1;
+                        callback(ContentsEvent::Progress(count));
+                        let file = prefix(i.0);
+                        let r = (pkg.to_string(), file);
+                        if !res.contains(&r) {
+                            res.push(r);
+                        }
+                    }
+                }
+            }
+
+            Some(())
+        });
+    }
+
+    callback(ContentsEvent::Done);
+
+    Ok(res)
+}
+
+#[cfg(feature = "no-rg-binary")]
+fn read_contents(path: &Path, search_zip: bool) -> Result<String> {
+    use flate2::bufread::GzDecoder;
+    use lzzzz::lz4f::BufReadDecompressor;
+    use std::io::Read;
+    let f = std::fs::File::open(path)?;
+    let mut contents_entry: Box<dyn Read> = match path.extension().and_then(|x| x.to_str()) {
+        Some("gz") if search_zip => Box::new(GzDecoder::new(BufReader::new(f))),
+        Some("lz4") if search_zip => Box::new(BufReadDecompressor::new(BufReader::new(f))?),
+        Some(_) | None => Box::new(BufReader::new(f)),
+    };
+
+    let mut s = String::new();
+    contents_entry.read_to_string(&mut s)?;
+
+    Ok(s)
+}
+
 /// Parse contents line
-fn parse_line(mut line: &str, is_list: bool, kw: &str) -> Option<(String, String)> {
+#[cfg(not(feature = "no-rg-binary"))]
+fn search_line(mut line: &str, is_list: bool, kw: &str) -> Option<(String, String)> {
     let (file, pkgs) = single_line::<()>(&mut line).ok()?;
 
     if pkgs.len() != 1 {
@@ -316,9 +352,7 @@ fn pkg_name_sep<'a, E: ParserError<&'a str>>(input: &mut &'a str) -> PResult<(),
 
 #[inline]
 fn second_single<'a, E: ParserError<&'a str>>(input: &mut &'a str) -> PResult<&'a str, E> {
-    let (res, _) = (take_till0(','), opt('\n')).parse_next(input)?;
-
-    Ok(res)
+    take_till0(|c| c == ',' || c == '\n').parse_next(input)
 }
 
 #[inline]
@@ -337,10 +371,19 @@ fn sep<'a, E: ParserError<&'a str>>(input: &mut &'a str) -> PResult<(), E> {
 }
 
 type ContentsLine<'a> = (&'a str, Vec<(&'a str, &'a str)>);
+#[cfg(feature = "no-rg-binary")]
+type ContentsLines<'a> = Vec<(&'a str, Vec<(&'a str, &'a str)>)>;
 
 #[inline]
 fn single_line<'a, E: ParserError<&'a str>>(input: &mut &'a str) -> PResult<ContentsLine<'a>, E> {
     separated_pair(first, sep, second).parse_next(input)
+}
+
+#[cfg(feature = "no-rg-binary")]
+#[inline]
+fn multi_line<'a, E: ParserError<&'a str>>(input: &mut &'a str) -> PResult<ContentsLines<'a>, E> {
+    use winnow::combinator::{repeat, terminated};
+    repeat(1.., terminated(single_line, tag("\n"))).parse_next(input)
 }
 
 #[inline]
@@ -350,7 +393,7 @@ fn prefix(s: &str) -> String {
 
 #[test]
 fn test_pkg_name() {
-    let a = &mut "admin/apt-file\n";
+    let a = &mut "admin/apt-file\n/   qaq/qaq\n";
     let res = pkg_split::<()>(a);
 
     assert_eq!(res, Ok(("admin", "apt-file")))
@@ -358,7 +401,7 @@ fn test_pkg_name() {
 
 #[test]
 fn test_second_single() {
-    let a = &mut "admin/apt-file,admin/apt\n";
+    let a = &mut "admin/apt-file\n/   qaq/qaq\n";
     let res = second_single::<()>(a);
     assert_eq!(res, Ok("admin/apt-file"));
 
@@ -373,21 +416,54 @@ fn test_second() {
     let res = second::<()>(a);
     assert_eq!(res, Ok(vec![("admin", "apt-file"), ("admin", "apt")]));
 
-    let b = &mut "admin/apt-file";
+    let b = &mut "admin/apt-file\n";
     let res = second::<()>(b);
     assert_eq!(res, Ok(vec![("admin", "apt-file")]));
 }
 
 #[test]
 fn test_single_line() {
-    let a = &mut "/   admin/apt-file,admin/apt\n";
+    let a = &mut "opt/32/libexec   devel/gcc+32,devel/llvm+32,gnome/gconf+32,libs/gdk-pixbuf+32\nopt/32/share   devel/llvm+32,libs/alsa-plugins+32,x11/libxkbcommon+32,x11/mesa+32,x11/virtualgl+32";
     let res = single_line::<()>(a);
     assert_eq!(
         res,
-        Ok(("/", vec![("admin", "apt-file"), ("admin", "apt")]))
+        Ok((
+            "opt/32/libexec",
+            vec![
+                ("devel", "gcc+32"),
+                ("devel", "llvm+32"),
+                ("gnome", "gconf+32"),
+                ("libs", "gdk-pixbuf+32")
+            ]
+        ))
     );
 
-    let b = &mut "/   admin/apt-file";
+    let b = &mut "/   admin/apt-file\n";
     let res = single_line::<()>(b);
     assert_eq!(res, Ok(("/", vec![("admin", "apt-file")])));
+}
+
+#[cfg(feature = "no-rg-binary")]
+#[test]
+fn test_multiple_lines() {
+    let a = &mut "opt/32/libexec   devel/gcc+32,devel/llvm+32,gnome/gconf+32\nopt/32/share   devel/llvm+32,libs/alsa-plugins+32\n";
+    let res = multi_line::<()>(a);
+
+    assert_eq!(
+        res,
+        Ok(vec![
+            (
+                "opt/32/libexec",
+                vec![
+                    ("devel", "gcc+32"),
+                    ("devel", "llvm+32"),
+                    ("gnome", "gconf+32"),
+                ]
+            ),
+            (
+                "opt/32/share",
+                vec![("devel", "llvm+32"), ("libs", "alsa-plugins+32")]
+            )
+        ])
+    )
 }
