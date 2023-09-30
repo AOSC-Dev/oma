7c8577501ab48e111577df3f68e7e1faa2fe39a6
eatradish <sakiiily@aosc.io>
2023-09-30 14:03:25 +0800

    fix: drop useless error enum

--- src/error.rs
+++ src/error.rs
@@ -219,7 +219,6 @@ fn oma_topics_error(e: OmaTopicsError) -> (String, Option<String>) {
             (fl!("can-not-find-specified-topic", topic = topic), None)
         }
         OmaTopicsError::ReqwestError(e) => (e.to_string(), Some(fl!("check-network-settings"))),
-        OmaTopicsError::SoutceListError(e) => (e.to_string(), Some(fl!("check-sources-list"))),
     }
 }
 
