32b9076f7370a78263099c3d6a5c273dc056da6d
eatradish <sakiiily@aosc.io>
2023-10-28 21:19:25 +0800

    refactor(oma-topics)!: improve error type

--- src/error.rs
+++ src/error.rs
@@ -209,7 +209,7 @@ impl From<OmaTopicsError> for OutputError {
 #[cfg(feature = "aosc")]
 fn oma_topics_error(e: OmaTopicsError) -> (String, Option<String>) {
     match e {
-        OmaTopicsError::SerdeError(_) => (fl!("failed-to-read"), Some(fl!("bug"))),
+        OmaTopicsError::BrokenFile(_) => (fl!("failed-to-read"), None),
         OmaTopicsError::IOError(e) => (OutputError::from(e).0 .0.to_string(), None),
         OmaTopicsError::CanNotFindTopic(topic) => (
             fl!("can-not-find-specified-topic", topic = topic),
@@ -219,6 +219,7 @@ fn oma_topics_error(e: OmaTopicsError) -> (String, Option<String>) {
             (fl!("can-not-find-specified-topic", topic = topic), None)
         }
         OmaTopicsError::ReqwestError(e) => (e.to_string(), Some(fl!("check-network-settings"))),
+        OmaTopicsError::FailedSer => (e.to_string(), None)
     }
 }
 
