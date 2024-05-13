d9287e3378e7de290ebfd389f672d07af2624c60
eatradish <sakiiily@aosc.io>
2024-05-13 22:42:45 +0800

    fix(oma-refresh): handle oma refresh download mirror file contains '+' ...
    ... apt will encode '+' twice (why?????), so hack it ...

--- oma-refresh/src/util.rs
+++ oma-refresh/src/util.rs
@@ -1,3 +1,19 @@
 pub(crate) fn database_filename(url: &str) -> String {
-    url.split("://").nth(1).unwrap_or(url).replace('/', "_")
+    url.split("://")
+        .nth(1)
+        .unwrap_or(url)
+        .replace('/', "_")
+        .replace('+', "%252b")
+}
+
+#[test]
+fn test_database_filename() {
+    // Mirror name contains '+' must be url encode twice
+    let s = "https://repo.aosc.io/debs/dists/x264-0+git20240305/InRelease";
+    let res = database_filename(s);
+
+    assert_eq!(
+        res,
+        "repo.aosc.io_debs_dists_x264-0%252bgit20240305_InRelease"
+    )
 }
