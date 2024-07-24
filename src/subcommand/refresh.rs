87cd1577d1c0881da79cde382dc1891d2f61e5fa
eatradish <sakiiily@aosc.io>
2024-07-24 20:52:43 +0800

    feat: add progress spinner for `oma refresh`

--- src/subcommand/refresh.rs
+++ src/subcommand/refresh.rs
@@ -1,3 +1,5 @@
+use oma_console::indicatif::ProgressBar;
+use oma_console::pb::oma_spinner;
 use oma_console::success;
 use oma_pm::apt::{OmaApt, OmaAptArgsBuilder};
 use reqwest::Client;
@@ -29,7 +31,16 @@ pub fn execute(oma_args: OmaArgs, sysroot: String, client: Client) -> Result<i32
 
     let oma_apt_args = OmaAptArgsBuilder::default().sysroot(sysroot).build()?;
     let apt = OmaApt::new(vec![], oma_apt_args, false)?;
+
+    let (style, inv) = oma_spinner(false);
+
+    let pb = ProgressBar::new_spinner().with_style(style);
+    pb.enable_steady_tick(inv);
+    pb.set_message(fl!("reading-database"));
+    
     let (upgradable, removable) = apt.available_action()?;
+    pb.finish_and_clear();
+
     let mut s = vec![];
 
     if upgradable != 0 {
