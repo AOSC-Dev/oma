5ab4a89698566fb7c2a50ab1230dcec6c3081135
eatradish <sakiiily@aosc.io>
2024-07-26 16:54:02 +0800

    feat: `oma topics` list topic name will wrap by `()`

--- src/subcommand/topics.rs
+++ src/subcommand/topics.rs
@@ -234,8 +234,7 @@ async fn inquire(
 
             if let Some(desc) = &x.description {
                 s += &style(desc).bold().to_string();
-                s.push(' ');
-                s += &x.name;
+                s += &format!(" ({})", x.name);
             } else {
                 s += &style(&x.name).bold().to_string();
             }
