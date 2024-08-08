7d41a4b2bae672fe1bb942542f68d432c10a3be1
eatradish <sakiiily@aosc.io>
2024-08-08 14:11:52 +0800

    fix(oma-refresh): fix database file name on flat repo ...
    ... and add test

--- oma-refresh/src/util.rs
+++ oma-refresh/src/util.rs
@@ -1,24 +1,29 @@
 use std::{borrow::Cow, path::Path};
 
-use oma_apt_sources_lists::{SourceLine, SourceListType, SourcesLists};
+use oma_apt_sources_lists::{SourceEntry, SourceLine, SourceListType, SourcesLists};
+use oma_utils::dpkg::dpkg_arch;
 use url::Url;
 
-use crate::db::{OmaSourceEntry, RefreshError};
+use crate::db::RefreshError;
 
 pub(crate) fn database_filename(url: &str) -> Result<String, RefreshError> {
     let url_parsed = Url::parse(url).map_err(|_| RefreshError::InvaildUrl(url.to_string()))?;
 
     let host = url_parsed.host_str();
 
+    // 不能使用 url_parsed.path()
+    // 原因是 "/./" 会被解析器解析为 "/"，而 apt 则不会这样
     let path = if let Some(host) = host {
-        // 不能使用 url_parsed.path()
-        // 原因是 "/./" 会被解析器解析为 "/"，而 apt 则不会这样
         url.split_once(host)
             .ok_or_else(|| RefreshError::InvaildUrl(url.to_string()))?
             .1
     } else {
-        // file 协议下不会有 host
-        url_parsed.path()
+        // file:/// or file:/
+        let res = url
+            .strip_prefix("file://")
+            .or(url.strip_prefix("file:"))
+            .ok_or_else(|| RefreshError::InvaildUrl(url.to_string()))?;
+        res
     };
 
     let url = if let Some(host) = host {
@@ -85,6 +90,121 @@ pub(crate) fn get_sources<P: AsRef<Path>>(sysroot: P) -> Result<Vec<OmaSourceEnt
     Ok(res)
 }
 
+#[derive(Debug, Clone)]
+pub(crate) struct OmaSourceEntry {
+    pub from: OmaSourceEntryFrom,
+    pub components: Vec<String>,
+    pub url: String,
+    pub suite: String,
+    pub dist_path: String,
+    pub is_flat: bool,
+    pub signed_by: Option<String>,
+    pub archs: Vec<String>,
+    pub trusted: bool,
+}
+
+#[derive(PartialEq, Eq, Debug, Clone)]
+pub enum OmaSourceEntryFrom {
+    Http,
+    Local,
+}
+
+impl OmaSourceEntry {
+    pub(crate) fn new(v: &SourceEntry, sysroot: impl AsRef<Path>) -> Result<Self, RefreshError> {
+        let from = if v.url().starts_with("http://") || v.url().starts_with("https://") {
+            OmaSourceEntryFrom::Http
+        } else if v.url().starts_with("file:") {
+            OmaSourceEntryFrom::Local
+        } else {
+            return Err(RefreshError::UnsupportedProtocol(format!("{v:?}")));
+        };
+
+        let components = v.components.clone();
+        let arch = &dpkg_arch(sysroot)?;
+        let url = v.url.replace("$(ARCH)", arch);
+        let suite = v.suite.replace("$(ARCH)", arch);
+
+        let (dist_path, is_flat) = if components.is_empty() {
+            // flat repo suite 后面一定有斜线
+            if url.ends_with('/') {
+                if suite == "/" {
+                    (url.to_string(), true)
+                } else {
+                    (format!("{}{}", url, suite), true)
+                }
+            } else {
+                (format!("{}/{}", url, suite), true)
+            }
+        } else {
+            (v.dist_path(), false)
+        };
+
+        let mut signed_by = None;
+        let mut archs = vec![];
+
+        let mut trusted = false;
+
+        for i in &v.options {
+            if let Some(v) = i.strip_prefix("arch=") {
+                for i in v.split(',') {
+                    archs.push(i.to_string());
+                }
+            }
+
+            if let Some(v) = i.strip_prefix("signed-by=") {
+                signed_by = Some(v.to_string());
+            }
+
+            if let Some(v) = i.strip_prefix("trusted=") {
+                trusted = v == "yes";
+            }
+        }
+
+        Ok(Self {
+            from,
+            components,
+            url,
+            suite,
+            is_flat,
+            dist_path,
+            signed_by,
+            archs,
+            trusted,
+        })
+    }
+}
+
+#[test]
+fn test_ose() {
+    let entry = SourceEntry {
+        enabled: true,
+        source: false,
+        options: vec![],
+        url: "file:///debs/".to_string(),
+        suite: "/".to_string(),
+        components: vec![],
+        is_deb822: false,
+    };
+
+    let ose = OmaSourceEntry::new(&entry, "/").unwrap();
+    assert_eq!(ose.url, "file:///debs/");
+    assert_eq!(ose.dist_path, "file:///debs/");
+
+    let entry = SourceEntry {
+        enabled: true,
+        source: false,
+        options: vec![],
+        url: "file:///debs/".to_string(),
+        suite: "./".to_string(),
+        components: vec![],
+        is_deb822: false,
+    };
+
+    let ose = OmaSourceEntry::new(&entry, "/").unwrap();
+    assert_eq!(ose.url, "file:///debs/");
+    assert_eq!(ose.dist_path, "file:///debs/./");
+}
+
 #[test]
 fn test_database_filename() {
     // Mirror name contains '+' must be url encode twice
@@ -107,4 +227,12 @@ fn test_database_filename() {
     let res2 = database_filename(s2).unwrap();
     assert_eq!(res1, "_debs");
     assert_eq!(res1, res2);
+
+    let s = "file:///debs/Packages";
+    let res = database_filename(s).unwrap();
+    assert_eq!(res, "_debs_Packages");
+
+    let s = "file:///debs//./Packages";
+    let res = database_filename(s).unwrap();
+    assert_eq!(res, "_debs__._Packages");
 }
