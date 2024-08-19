ed38ca66ebe961094ab414c3f13d03703f51b0be
eatradish <sakiiily@aosc.io>
2024-08-19 13:30:50 +0800

    fix(oma-console): fix global progress bar align

--- oma-console/src/pb.rs
+++ oma-console/src/pb.rs
@@ -1,6 +1,7 @@
 use std::{fmt::Write, time::Duration};
 
-use indicatif::{BinaryBytes, HumanBytes, ProgressState, ProgressStyle};
+use console::style;
+use indicatif::{HumanBytes, ProgressState, ProgressStyle};
 
 use crate::writer::Writer;
 
@@ -20,13 +21,13 @@ const SPINNER: &[&str] = &[
 pub fn progress_bar_style(writer: &Writer) -> ProgressStyle {
     let max_len = writer.get_length();
     let template = if max_len < 100 {
-        " {msg} {percent:>3}".to_owned()
+        " {msg} {percent:>3}"
     } else {
-        " {msg:<59} {total_bytes:<11} [{wide_bar:.white/black}] {percent:>3}".to_owned()
+        " {msg:<59} {total_bytes:<11} [{wide_bar:.white/black}] {percent:>3}"
     };
 
     ProgressStyle::default_bar()
-        .template(&template)
+        .template(template)
         .unwrap()
         .progress_chars("=>-")
         .with_key("percent", |state: &ProgressState, w: &mut dyn Write| {
@@ -37,27 +38,45 @@ pub fn progress_bar_style(writer: &Writer) -> ProgressStyle {
 pub fn global_progress_bar_style(writer: &Writer) -> ProgressStyle {
     let max_len = writer.get_length();
     let template = if max_len < 100 {
-        " {prefix:.blue.bold} {bytes:>14.green.bold} {total_bytes:.green.bold} {binary_bytes_per_sec:<10.green.bold}".to_owned()
+        " {prefix:.blue.bold} {bytes:>14.green.bold} {total_bytes:.green.bold} {binary_bytes_per_sec:<10.green.bold}"
     } else {
-        " {prefix:.blue.bold} {bytes:>14.green.bold} {total_bytes:>8.green.bold} {binary_bytes_per_sec:<25.green.bold} {eta_precise:<10.blue.bold} [{wide_bar:.blue.bold}] {percent:>3.blue.bold}".to_owned()
+        " {progress_msg:<59} {elapsed_precise:<11.blue.bold} [{wide_bar:.cyan/blue}] {percent:>3}"
     };
 
     ProgressStyle::default_bar()
-        .template(&template)
+        .template(template)
         .unwrap()
         .progress_chars("=>-")
-        .with_key("bytes", |state: &ProgressState, w: &mut dyn Write| {
-            write!(w, "{} /", HumanBytes(state.pos())).unwrap()
-        })
-        .with_key(
-            "binary_bytes_per_sec",
-            |state: &ProgressState, w: &mut dyn Write| {
-                write!(w, "@ {}/s", BinaryBytes(state.per_sec() as u64)).unwrap()
-            },
-        )
         .with_key("percent", |state: &ProgressState, w: &mut dyn Write| {
             write!(w, "{:.*}%", 0, state.fraction() * 100f32).unwrap()
         })
+        .with_key("progress_msg", oma_global_bar_template)
+}
+
+fn oma_global_bar_template(state: &ProgressState, w: &mut dyn Write) {
+    write!(w, "{}  ", style("Progress").blue().bold()).unwrap();
+
+    write!(
+        w,
+        "{}",
+        style(format!(
+            "{} / {}",
+            HumanBytes(state.pos()),
+            HumanBytes(state.len().unwrap_or(0))
+        ))
+        .green()
+        .bold()
+    )
+    .unwrap();
+
+    write!(
+        w,
+        "{}",
+        style(format!(" @ {}/s", HumanBytes(state.per_sec() as u64)))
+            .green()
+            .bold()
+    )
+    .unwrap();
 }
 
 pub fn spinner_style() -> (ProgressStyle, Duration) {
