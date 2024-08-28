2a2c9d76b33a0619e94541a9aa517cbb8668d056
eatradish <sakiiily@aosc.io>
2024-08-28 23:34:26 +0800

    chore(oma-contents): cleanup example code

--- oma-contents/examples/list_files_rg.rs
+++ oma-contents/examples/list_files_rg.rs
@@ -1,13 +1,6 @@
-use std::time::Duration;
-
-use indicatif::ProgressBar;
 use oma_contents::searcher::{ripgrep_search, Mode};
 
 fn main() {
-    let pb = ProgressBar::new_spinner();
-    pb.enable_steady_tick(Duration::from_millis(100));
-    pb.set_message("Searching ...");
-
     ripgrep_search("/var/lib/apt/lists", Mode::Files, "apt", |(pkg, file)| {
         println!("{pkg}: {file}")
     })
