a1d67aeee7328d7c18a57e81debcac20eb429e85
Mag Mell <qingbaodou@gmail.com>
2024-10-23 11:42:41 +0800

    refactor: use `Option::take` to replace get val after set `None` (#172)

--- src/pb.rs
+++ src/pb.rs
@@ -59,9 +59,8 @@ impl Writeln for OmaProgressBar {
                 MessageType::Prefix => output.0 = Some(gen_prefix(s, 10)),
             }
 
-            if let (Some(prefix), Some(msg)) = &output {
+            if let (Some(prefix), Some(msg)) = (output.0.take(), output.1.take()) {
                 self.inner.println(format!("{prefix}{msg}"));
-                output = (None, None);
             }
         });
 
