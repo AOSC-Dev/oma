45491d7c059b78795db6575fb9948f24b995326d
eatradish <sakiiily@aosc.io>
2024-11-22 11:10:18 +0800

    feat(oma-console): disable ansi output with `--debug`, `--no-color` or not is terminal

--- oma-console/examples/msg.rs
+++ oma-console/examples/msg.rs
@@ -2,7 +2,7 @@ use oma_console::{due_to, success, OmaLayer};
 use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt};
 
 fn main() {
-    tracing_subscriber::registry().with(OmaLayer).init();
+    tracing_subscriber::registry().with(OmaLayer::new()).init();
     tracing::info!("Welcome");
     tracing::debug!("Hello");
     tracing::info!("I'am fine");
