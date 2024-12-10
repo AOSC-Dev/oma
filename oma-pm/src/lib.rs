e1b602b9f166e28ecc1edc653e530d26434adb1a
wxiwnd <wxiwnd@outlook.com>
2024-12-10 16:01:38 +0800

    add: doc for oma-pm lib
    Signed-off-by: wxiwnd <wxiwnd@outlook.com>

--- oma-pm/src/lib.rs
+++ oma-pm/src/lib.rs
@@ -1,3 +1,32 @@
+//! # oma-pm
+//!
+//! The package manager component for oma.
+//!
+//! The oma-pm crate serves as the main library for oma,
+//! providing essential functionalities for package management operations such as install, remove, upgrade, and search.
+//!
+//! ## Features
+//!
+//! 1. **PackageInfo**: Provides definitions of package information and handle them to `rust-apt`.
+//! 2. **Progress**: Tracks the progress of package management operations.
+//! 3. **Search Result**: Defines the structure and handling of search results.
+//! 4. **Communication with rust-apt**: Communicate with `rust-apt`.
+//!
+//! ## Modules
+//!
+//! - `apt`: Handles interactions with `apt`.
+//! - `matches`: Provides utilities for matching package information.
+//! - `pkginfo`: Contains definitions and structures for package information.
+//! - `progress`: Tracks the progress of package management operations.
+//! - `search`: Defines the structure and handling of search results.
+//! - `dbus`: Manages D-Bus communication.
+//!
+//! ## Re-exports
+//!
+//! - `AptErrors`: Error definitions from `oma_apt` crate.
+//! - `PkgCurrentState`: Current state definitions from `oma_apt` crate.
+//! - `PackageStatus`: Status definitions from the `search` module.
+
 pub mod apt;
 pub mod matches;
 pub mod pkginfo;
