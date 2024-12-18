084e826ad7ed96ae997ebd1a6fb520ef50d17a78
eatradish <sakiiily@aosc.io>
2024-12-18 23:34:15 +0800

    fix(oma-refresh): do not unwrap on `modify_result`

--- oma-refresh/src/config.rs
+++ oma-refresh/src/config.rs
@@ -30,8 +30,12 @@ fn modify_result(
             }
         }
 
+        let Some(tag) = node.tag() else {
+            continue;
+        };
+
         if let Some(entry) = node.child() {
-            stack.push_back((entry, format!("{}::{}", tree_path, node.tag().unwrap())));
+            stack.push_back((entry, format!("{}::{}", tree_path, tag)));
         }
 
         if let Some((k, v)) = node.tag().zip(node.value()) {
