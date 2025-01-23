fff64fcd07f644d54ac0dce1baf0711a4a1f86cb
Mag Mell <qingbaodou@gmail.com>
2025-01-23 16:50:23 +0800

    fix(list): fix `oma list` get upgradable item will panic (#337)

--- src/subcommand/list.rs
+++ src/subcommand/list.rs
@@ -215,12 +215,9 @@ impl CliExecuter for List {
                             "{}/{} {} {arch} {s}",
                             color_formatter().color_str(&name, Action::Emphasis).bold(),
                             color_formatter().color_str(branches_str, Action::Secondary),
-                            if upgradable {
-                                version_str = Cow::Owned(format!(
-                                    "{} -> {}",
-                                    version_str,
-                                    new_version.as_ref().unwrap(),
-                                ));
+                            if let Some(new_version) = new_version {
+                                version_str =
+                                    Cow::Owned(format!("{} -> {}", version_str, new_version));
                                 color_formatter().color_str(version_str, Action::WARN)
                             } else {
                                 color_formatter().color_str(version_str, Action::EmphasisSecondary)
