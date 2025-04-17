fd6acc6425b9e1fc0ca272d7d043d2aeba66ff7d
eatradish <sakiiily@aosc.io>
2025-04-17 15:31:33 +0800

    feat(clean): add free up size tips

--- src/subcommand/clean.rs
+++ src/subcommand/clean.rs
@@ -1,10 +1,13 @@
 use std::fs::{self};
+use std::io::ErrorKind;
 use std::path::{Path, PathBuf};
 
 use crate::subcommand::utils::create_progress_spinner;
 use crate::{config::Config, fl};
 use crate::{error, success};
 use clap::Args;
+use fs_extra::dir::get_size;
+use oma_console::indicatif::HumanBytes;
 use oma_pm::apt::{AptConfig, OmaApt, OmaAptArgs};
 use tracing::{debug, info};
 
@@ -67,14 +70,16 @@ impl CliExecuter for Clean {
 
         let pb = create_progress_spinner(no_progress, fl!("cleaning"));
 
-        remove(&download_dir.join("partial"));
+        let mut total_clean_size = 0;
+
+        remove(&download_dir.join("partial"), &mut total_clean_size);
 
         for i in dir
             .flatten()
             .filter(|x| x.path().extension().is_some_and(|name| name == "deb"))
         {
             if !keep_downloadable && !keep_downloadable_and_installed && !keep_installed {
-                remove(&i.path());
+                remove(&i.path(), &mut total_clean_size);
                 continue;
             }
 
@@ -92,14 +97,14 @@ impl CliExecuter for Clean {
                     "Failed to get package name or version: {}, will delete this file",
                     i.path().display()
                 );
-                remove(&i.path());
+                remove(&i.path(), &mut total_clean_size);
                 continue;
             };
 
             let version = version.replace("%3a", ":");
 
             let Some(version) = apt.cache.get(pkg).and_then(|pkg| pkg.get_version(&version)) else {
-                remove(&i.path());
+                remove(&i.path(), &mut total_clean_size);
                 continue;
             };
 
@@ -107,14 +112,14 @@ impl CliExecuter for Clean {
                 && !version.is_downloadable()
                 && keep_downloadable_and_installed
             {
-                remove(&i.path());
+                remove(&i.path(), &mut total_clean_size);
                 continue;
             }
 
             if (!version.is_downloadable() && keep_downloadable)
                 || (!version.is_installed() && keep_installed)
             {
-                remove(&i.path());
+                remove(&i.path(), &mut total_clean_size);
                 continue;
             }
         }
@@ -123,25 +128,34 @@ impl CliExecuter for Clean {
             pb.inner.finish_and_clear();
         }
 
-        success!("{}", fl!("clean-successfully"));
+        let size = HumanBytes(total_clean_size).to_string();
+        success!("{}", fl!("clean-successfully", size = size));
 
         Ok(0)
     }
 }
 
-fn remove(i: &Path) {
+fn remove(i: &Path, total_size: &mut u64) {
+    let size = get_size(i);
+
     let result = if i.is_dir() {
         fs::remove_dir(i)
     } else {
         fs::remove_file(i)
     };
 
-    if let Err(e) = result {
-        error!(
-            "Failed to delete {} {}: {}",
-            if i.is_dir() { "dir" } else { "file" },
-            i.display(),
-            e
-        );
+    match result {
+        Ok(_) => *total_size += size.unwrap_or(0),
+        Err(e) => {
+            if e.kind() == ErrorKind::NotFound {
+                return;
+            }
+            error!(
+                "Failed to delete {} {}: {}",
+                if i.is_dir() { "dir" } else { "file" },
+                i.display(),
+                e
+            );
+        }
     }
 }
