78b439a09bc8114071df7560975f39793f477e16
Mag Mell <qingbaodou@gmail.com>
2025-05-16 17:37:18 +0800

    fix(history): make `--no-refresh-topics` aosc only (#437)

--- src/subcommand/history.rs
+++ src/subcommand/history.rs
@@ -110,6 +110,7 @@ pub struct Undo {
     /// Do not refresh repository metadata
     #[arg(long)]
     no_refresh: bool,
+    #[cfg(feature = "aosc")]
     /// Do not refresh topics manifest.json file
     #[arg(long)]
     no_refresh_topics: bool,
@@ -134,6 +135,7 @@ impl CliExecuter for Undo {
             no_fix_dpkg_status,
             download_threads,
             no_refresh,
+            #[cfg(feature = "aosc")]
             no_refresh_topics,
         } = self;
 
