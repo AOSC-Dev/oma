ca6248abcfad9c35defc1b34c4f62f717e4ab7d5
eatradish <sakiiily@aosc.io>
2025-08-05 11:24:40 +0800

    fmt

--- oma-mirror/examples/parse_template.rs
+++ oma-mirror/examples/parse_template.rs
@@ -1,14 +1,12 @@
 use oma_mirror::parser::{MirrorsConfigTemplate, TemplateParseError};
 
 fn main() -> Result<(), TemplateParseError> {
-    let t = MirrorsConfigTemplate::parse_from_file(
-        "./oma-mirror/examples/mirror_templates/1.toml",
-    )?;
+    let t =
+        MirrorsConfigTemplate::parse_from_file("./oma-mirror/examples/mirror_templates/1.toml")?;
     dbg!(t);
 
-    let t = MirrorsConfigTemplate::parse_from_file(
-        "./oma-mirror/examples/mirror_templates/2.toml",
-    )?;
+    let t =
+        MirrorsConfigTemplate::parse_from_file("./oma-mirror/examples/mirror_templates/2.toml")?;
     dbg!(t);
 
     Ok(())
