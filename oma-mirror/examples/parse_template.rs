fe05bb41f322c0e0dedbdbd80910437715477b38
Mag Mell <qingbaodou@gmail.com>
2025-08-07 21:51:12 +0800

    chore(oma-mirror): use `CARGO_MANIFEST_DIR` env (#507)

--- oma-mirror/examples/parse_template.rs
+++ oma-mirror/examples/parse_template.rs
@@ -1,12 +1,16 @@
+use std::path::Path;
+
 use oma_mirror::parser::{MirrorsConfigTemplate, TemplateParseError};
 
 fn main() -> Result<(), TemplateParseError> {
-    let t =
-        MirrorsConfigTemplate::parse_from_file("./oma-mirror/examples/mirror_templates/1.toml")?;
+    let dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+    let p1 = dir.join("examples/mirror_templates/1.toml");
+    let p2 = dir.join("examples/mirror_templates/2.toml");
+
+    let t = MirrorsConfigTemplate::parse_from_file(p1)?;
     dbg!(t);
 
-    let t =
-        MirrorsConfigTemplate::parse_from_file("./oma-mirror/examples/mirror_templates/2.toml")?;
+    let t = MirrorsConfigTemplate::parse_from_file(p2)?;
     dbg!(t);
 
     Ok(())
