5595d2bd254f5dfdc4d4b55f432bb5373370a7a9
eatradish <sakiiily@aosc.io>
2025-08-07 12:02:32 +0800

    feat(oma-tum): impl version v2 parser

--- oma-tum/src/lib.rs
+++ oma-tum/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod parser;
 use std::{
     fs::{self, read_dir},
     io,
@@ -5,10 +6,13 @@ use std::{
 };
 
 use ahash::{HashMap, HashSet};
+use debversion::Version;
 use oma_pm_operation_type::{InstallOperation, OmaOperation};
 use serde::Deserialize;
 use snafu::{ResultExt, Snafu};
 use tracing::warn;
+
+use crate::parser::{VersionParseError, VersionToken, parse_version_expr};
 
 #[derive(Deserialize, Debug)]
 pub struct TopicUpdateManifest {
@@ -27,7 +31,10 @@ pub enum TopicUpdateEntry {
     #[serde(rename = "conventional")]
     Conventional {
         security: bool,
+        #[serde(default)]
         packages: HashMap<String, Option<String>>,
+        #[serde(default)]
+        packages_v2: HashMap<String, Option<String>>,
         name: HashMap<String, String>,
         caution: Option<HashMap<String, String>>,
         #[serde(default = "must_match_all_default")]
@@ -48,6 +55,7 @@ pub enum TopicUpdateEntryRef<'a> {
     Conventional {
         security: bool,
         packages: &'a HashMap<String, Option<String>>,
+        packages_v2: &'a HashMap<String, Option<String>>,
         name: &'a HashMap<String, String>,
         caution: Option<&'a HashMap<String, String>>,
     },
@@ -70,7 +78,17 @@ impl TopicUpdateEntryRef<'_> {
 
     pub fn count_packages(&self) -> usize {
         match self {
-            TopicUpdateEntryRef::Conventional { packages, .. } => packages.len(),
+            TopicUpdateEntryRef::Conventional {
+                packages,
+                packages_v2,
+                ..
+            } => {
+                if !packages_v2.is_empty() {
+                    packages_v2.len()
+                } else {
+                    packages.len()
+                }
+            }
             TopicUpdateEntryRef::Cumulative {
                 count_packages_changed,
                 ..
@@ -87,10 +105,12 @@ impl<'a> From<&'a TopicUpdateEntry> for TopicUpdateEntryRef<'a> {
                 packages,
                 name,
                 caution,
+                packages_v2,
                 ..
             } => TopicUpdateEntryRef::Conventional {
                 security: *security,
                 packages,
+                packages_v2,
                 name,
                 caution: caution.as_ref(),
             },
@@ -118,6 +138,8 @@ pub enum TumError {
     ReadDirEntry { source: io::Error },
     #[snafu(display("Failed to read file: {}", path.display()))]
     ReadFile { path: PathBuf, source: io::Error },
+    #[snafu(display("Parse version expr got error"))]
+    ParseVersionExpr { source: VersionParseError },
 }
 
 pub fn get_tum(sysroot: &Path) -> Result<Vec<TopicUpdateManifest>, TumError> {
@@ -173,25 +195,50 @@ pub fn get_matches_tum<'a>(
             if let TopicUpdateEntry::Conventional {
                 must_match_all,
                 packages,
+                packages_v2,
                 ..
             } = entry
             {
-                'b: for (index, (pkg_name, version)) in packages.iter().enumerate() {
-                    if !must_match_all
-                        && (install_pkg_on_topic(install_map, pkg_name, version)
-                            || remove_pkg_on_topic(remove_map, pkg_name, version))
-                    {
-                        break 'b;
-                    } else if !install_pkg_on_topic(install_map, pkg_name, version)
-                        && !remove_pkg_on_topic(remove_map, pkg_name, version)
-                    {
-                        if *must_match_all || index == packages.len() - 1 {
-                            continue 'a;
-                        } else {
-                            continue 'b;
+                if !packages_v2.is_empty() {
+                    // v2
+                    'b: for (index, (pkg_name, version)) in packages_v2.iter().enumerate() {
+                        if !must_match_all
+                            && (install_pkg_on_topic_v2(install_map, pkg_name, version)
+                                .unwrap_or(false)
+                                || remove_pkg_on_topic(remove_map, pkg_name, version))
+                        {
+                            break 'b;
+                        } else if !install_pkg_on_topic_v2(install_map, pkg_name, version)
+                            .unwrap_or(false)
+                            && !remove_pkg_on_topic(remove_map, pkg_name, version)
+                        {
+                            if *must_match_all || index == packages_v2.len() - 1 {
+                                continue 'a;
+                            } else {
+                                continue 'b;
+                            }
+                        }
+                    }
+                } else if !packages.is_empty() {
+                    // v1
+                    'b: for (index, (pkg_name, version)) in packages.iter().enumerate() {
+                        if !must_match_all
+                            && (install_pkg_on_topic(install_map, pkg_name, version)
+                                || remove_pkg_on_topic(remove_map, pkg_name, version))
+                        {
+                            break 'b;
+                        } else if !install_pkg_on_topic(install_map, pkg_name, version)
+                            && !remove_pkg_on_topic(remove_map, pkg_name, version)
+                        {
+                            if *must_match_all || index == packages.len() - 1 {
+                                continue 'a;
+                            } else {
+                                continue 'b;
+                            }
                         }
                     }
                 }
+
                 matches.insert(name.as_str(), TopicUpdateEntryRef::from(entry));
             }
         }
@@ -206,11 +253,20 @@ pub fn get_matches_tum<'a>(
                     for t in topics {
                         let t = matches.remove(t.as_str()).unwrap();
 
-                        let TopicUpdateEntryRef::Conventional { packages, .. } = t else {
+                        let TopicUpdateEntryRef::Conventional {
+                            packages,
+                            packages_v2,
+                            ..
+                        } = t
+                        else {
                             unreachable!()
                         };
 
-                        count_packages_changed_tmp += packages.len();
+                        if !packages_v2.is_empty() {
+                            count_packages_changed_tmp += packages_v2.len();
+                        } else {
+                            count_packages_changed_tmp += packages.len();
+                        }
                     }
 
                     let mut entry = TopicUpdateEntryRef::from(entry);
@@ -239,7 +295,10 @@ pub fn collection_all_matches_security_tum_pkgs<'a>(
     let mut res = HashMap::with_hasher(ahash::RandomState::new());
     for v in matches_tum.values() {
         let TopicUpdateEntryRef::Conventional {
-            security, packages, ..
+            security,
+            packages,
+            packages_v2,
+            ..
         } = v
         else {
             continue;
@@ -249,11 +308,19 @@ pub fn collection_all_matches_security_tum_pkgs<'a>(
             continue;
         }
 
-        res.extend(
-            packages
-                .iter()
-                .map(|(pkg, version)| (pkg.as_str(), version)),
-        );
+        if !packages_v2.is_empty() {
+            res.extend(
+                packages_v2
+                    .iter()
+                    .map(|(pkg, version)| (pkg.as_str(), version)),
+            );
+        } else {
+            res.extend(
+                packages
+                    .iter()
+                    .map(|(pkg, version)| (pkg.as_str(), version)),
+            );
+        }
     }
 
     res
@@ -272,13 +339,98 @@ fn install_pkg_on_topic(
         return false;
     };
 
+    compare_version(install_ver, tum_version, VersionToken::Eq)
+}
+
+fn compare_version(install_ver: &str, tum_version: &str, op: VersionToken) -> bool {
     if let Some((prefix, suffix)) = install_ver.rsplit_once("~pre")
         && is_topic_preversion(suffix)
     {
-        return tum_version == prefix;
+        return compare_version_inner(prefix, tum_version, op);
     }
 
-    tum_version == install_ver
+    compare_version_inner(install_ver, tum_version, op)
+}
+
+fn compare_version_inner(another_ver: &str, tum_version: &str, op: VersionToken<'_>) -> bool {
+    let another_ver: Version = another_ver.parse().unwrap();
+    let tum_version: Version = tum_version.parse().unwrap();
+
+    match op {
+        VersionToken::Eq | VersionToken::EqEq => tum_version == another_ver,
+        VersionToken::NotEq => tum_version != another_ver,
+        VersionToken::GtEq => another_ver >= tum_version,
+        VersionToken::LtEq => another_ver <= tum_version,
+        VersionToken::Gt => another_ver > tum_version,
+        VersionToken::Lt => another_ver < tum_version,
+        _ => unreachable!(),
+    }
+}
+
+fn install_pkg_on_topic_v2(
+    install_map: &HashMap<&str, &str>,
+    pkg_name: &str,
+    tum_version: &Option<String>,
+) -> Result<bool, TumError> {
+    let Some(install_ver) = install_map.get(pkg_name) else {
+        return Ok(false);
+    };
+
+    let Some(tum_version_expr) = tum_version else {
+        return Ok(false);
+    };
+
+    let tokens = parse_version_expr(tum_version_expr).context(ParseVersionExprSnafu)?;
+
+    Ok(is_right_version(tokens, install_ver))
+}
+
+fn is_right_version(tokens: Vec<VersionToken<'_>>, install_ver: &str) -> bool {
+    let tokens: Vec<_> = tokens
+        .into_iter()
+        .map(|x| {
+            if let VersionToken::VersionNumber("$VER") = x {
+                VersionToken::VersionNumber(install_ver)
+            } else {
+                x
+            }
+        })
+        .collect();
+
+    let mut stack = vec![];
+    let mut index = 0;
+
+    while index < tokens.len() {
+        match tokens[index] {
+            VersionToken::VersionNumber(install_ver) => {
+                let VersionToken::VersionNumber(tum_version) = tokens[index + 1] else {
+                    unreachable!()
+                };
+
+                let b = compare_version(install_ver, tum_version, tokens[index + 2]);
+
+                stack.push(b);
+                index += 3;
+            }
+            VersionToken::Or => {
+                let b1 = stack.pop().unwrap();
+                let b2 = stack.pop().unwrap();
+                stack.push(b1 || b2);
+                index += 1;
+            }
+            VersionToken::And => {
+                let b1 = stack.pop().unwrap();
+                let b2 = stack.pop().unwrap();
+                stack.push(b1 && b2);
+                index += 1;
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    assert!(stack.len() == 1);
+
+    stack[0]
 }
 
 fn is_topic_preversion(suffix: &str) -> bool {
@@ -315,4 +467,35 @@ fn test_is_topic_preversion() {
     let suffix = "20241213T090405Z";
     let res = is_topic_preversion(suffix);
     assert!(res);
+}
+
+#[test]
+fn test_is_right_version() {
+    let input_expr = "(=1.2.3 || =4.5.6) && <7.8.9";
+    let ver1 = "4.5.6";
+    let ver2 = "1.2.3";
+    let tokens = parse_version_expr(input_expr).unwrap();
+    assert!(is_right_version(tokens.clone(), ver1));
+    assert!(is_right_version(tokens, ver2));
+
+    let input_expr = "<=7.8.9";
+    let ver1 = "1.2.3";
+    let ver2 = "7.8.9";
+    let tokens = parse_version_expr(input_expr).unwrap();
+    assert!(is_right_version(tokens.clone(), ver1));
+    assert!(is_right_version(tokens, ver2));
+
+    let input_expr = ">=7.8.9";
+    let ver1 = "11.0.0";
+    let ver2 = "7.8.9";
+    let tokens = parse_version_expr(input_expr).unwrap();
+    assert!(is_right_version(tokens.clone(), ver1));
+    assert!(is_right_version(tokens, ver2));
+
+    let input_expr = "=7.8.9";
+    let ver1 = "7.8.9";
+    let ver2 = "1.2.3";
+    let tokens = parse_version_expr(input_expr).unwrap();
+    assert!(is_right_version(tokens.clone(), ver1));
+    assert!(!is_right_version(tokens, ver2));
 }
---
+++ oma-tum/src/parser.rs
@@ -0,0 +1,219 @@
+use std::fmt::Display;
+
+use logos::{Logos, Span};
+use snafu::Snafu;
+
+#[derive(Logos, Copy, Clone, Debug, PartialEq)]
+#[logos(skip r"[ \t\n\f]+")] // ignore whitespace and newlines
+pub enum VersionToken<'source> {
+    #[token("=")]
+    Eq,
+    #[token("==")]
+    EqEq,
+    #[token("!=")]
+    NotEq,
+    #[token(">=")]
+    GtEq,
+    #[token("<=")]
+    LtEq,
+    #[token(">")]
+    Gt,
+    #[token("<")]
+    Lt,
+    #[token("||")]
+    Or,
+    #[token("&&")]
+    And,
+    #[token("(")]
+    LParen,
+    #[token(")")]
+    RParen,
+    #[regex(r"[a-fA-F0-9]+", priority = 3)]
+    Hexadecimal(&'source str),
+    #[regex(r"(\d+:)?[0-9][0-9A-Za-z.+\-~]*")]
+    VersionNumber(&'source str),
+}
+
+impl<'source> Display for VersionToken<'source> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            VersionToken::Eq => write!(f, "="),
+            VersionToken::EqEq => write!(f, "=="),
+            VersionToken::NotEq => write!(f, "!="),
+            VersionToken::GtEq => write!(f, ">="),
+            VersionToken::LtEq => write!(f, "<="),
+            VersionToken::Gt => write!(f, ">"),
+            VersionToken::Lt => write!(f, "<"),
+            VersionToken::Or => write!(f, "||"),
+            VersionToken::And => write!(f, "&&"),
+            VersionToken::LParen => write!(f, "("),
+            VersionToken::RParen => write!(f, ")"),
+            VersionToken::Hexadecimal(hex) => write!(f, "{hex}"),
+            VersionToken::VersionNumber(version) => write!(f, "{version}"),
+        }
+    }
+}
+
+impl<'source> VersionToken<'source> {
+    pub fn is_op(&self) -> bool {
+        matches!(
+            self,
+            VersionToken::Eq
+                | VersionToken::EqEq
+                | VersionToken::NotEq
+                | VersionToken::GtEq
+                | VersionToken::LtEq
+                | VersionToken::Gt
+                | VersionToken::Lt
+                | VersionToken::Or
+                | VersionToken::And
+        )
+    }
+
+    pub fn is_cmp_op(&self) -> bool {
+        matches!(
+            self,
+            VersionToken::Eq
+                | VersionToken::EqEq
+                | VersionToken::NotEq
+                | VersionToken::GtEq
+                | VersionToken::LtEq
+                | VersionToken::Gt
+                | VersionToken::Lt
+        )
+    }
+
+    pub fn precedence(&self) -> u8 {
+        match self {
+            VersionToken::Eq
+            | VersionToken::GtEq
+            | VersionToken::LtEq
+            | VersionToken::Gt
+            | VersionToken::Lt
+            | VersionToken::NotEq => 10,
+            VersionToken::Or | VersionToken::And => 1,
+            _ => 0, // invalid operator
+        }
+    }
+}
+
+// const ZERO_STRING: &'static str = "0";
+const VERSION_PLACEHOLDER: &str = "$VER";
+const VERSION_PLACEHOLDER_TOKEN: VersionToken = VersionToken::VersionNumber(VERSION_PLACEHOLDER);
+
+#[derive(Debug, Snafu)]
+pub enum VersionParseError {
+    #[snafu(display("Invalid version expression at position {span:?}"))]
+    VersionExpr { span: Span },
+    #[snafu(display("Unexpected string '{s}' at position {span:?}"))]
+    UnexpectedString { s: String, span: Span },
+    #[snafu(display("Unmatched '(' at position {span:?}"))]
+    UnmatchedLeft { span: Span },
+}
+
+pub fn parse_version_expr(input: &str) -> Result<Vec<VersionToken>, VersionParseError> {
+    let mut lexer = VersionToken::lexer(input);
+    let mut stack: Vec<VersionToken> = Vec::with_capacity(8);
+    let mut operators: Vec<VersionToken> = Vec::with_capacity(8);
+    let mut prev_is_op = false;
+
+    // convert infix notation to RPN
+    while let Some(maybe_token) = lexer.next() {
+        let token =
+            maybe_token.map_err(|_| VersionParseError::VersionExpr { span: lexer.span() })?;
+
+        if token.is_cmp_op() {
+            // since we use a very simplified expression format, we don't have a LHS in our "binary expression"
+            // we will push a dummy VERSION_PLACEHOLDER_TOKEN to the stack, and later replace it with the actual version
+            stack.push(VERSION_PLACEHOLDER_TOKEN);
+        }
+
+        match token {
+            VersionToken::Eq
+            | VersionToken::EqEq
+            | VersionToken::NotEq
+            | VersionToken::GtEq
+            | VersionToken::LtEq
+            | VersionToken::Gt
+            | VersionToken::Lt
+            | VersionToken::Or
+            | VersionToken::And => {
+                if let Some(last_op) = operators.last() {
+                    if last_op.precedence() >= token.precedence() {
+                        let last = operators.pop().unwrap();
+                        stack.push(last);
+                        operators.push(token);
+                        prev_is_op = token.is_op();
+                        continue;
+                    }
+                }
+                operators.push(token);
+            }
+            VersionToken::LParen => operators.push(token),
+            VersionToken::RParen => {
+                // drain all operators and push them back to the output stack
+                while let Some(op) = operators.pop() {
+                    if op == VersionToken::LParen {
+                        break;
+                    }
+                    stack.push(op);
+                }
+            }
+            VersionToken::Hexadecimal(_) => {
+                return Err(VersionParseError::VersionExpr { span: lexer.span() });
+            }
+            VersionToken::VersionNumber(_) => {
+                if !prev_is_op {
+                    return Err(VersionParseError::UnexpectedString {
+                        s: token.to_string(),
+                        span: lexer.span(),
+                    });
+                }
+                stack.push(token);
+            }
+        }
+
+        prev_is_op = token.is_op();
+    }
+
+    // drain all remaining operators and add them to the output stack
+    while let Some(op) = operators.pop() {
+        if op == VersionToken::LParen {
+            return Err(VersionParseError::UnmatchedLeft { span: lexer.span() });
+        }
+        stack.push(op);
+    }
+
+    Ok(stack)
+}
+
+#[test]
+fn test_lexer() {
+    let input = "1.2.3+4-5";
+    let mut lexer = VersionToken::lexer(input);
+    let token = lexer.next().unwrap();
+    assert_eq!(token, Ok(VersionToken::VersionNumber(input)));
+    assert_eq!(lexer.slice(), "1.2.3+4-5");
+}
+
+#[test]
+fn test_parser_simple() {
+    let input_expr = "(=1.2.3 || =4.5.6) && <7.8.9";
+    let tokens = parse_version_expr(input_expr).unwrap();
+    assert_eq!(
+        tokens,
+        vec![
+            VERSION_PLACEHOLDER_TOKEN,
+            VersionToken::VersionNumber("1.2.3"),
+            VersionToken::Eq,
+            VERSION_PLACEHOLDER_TOKEN,
+            VersionToken::VersionNumber("4.5.6"),
+            VersionToken::Eq,
+            VersionToken::Or,
+            VERSION_PLACEHOLDER_TOKEN,
+            VersionToken::VersionNumber("7.8.9"),
+            VersionToken::Lt,
+            VersionToken::And,
+        ]
+    );
+}
