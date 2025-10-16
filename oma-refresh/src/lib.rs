2454313d25b86abc1a63f51d8563af2300c52cc7
Mag Mell <qingbaodou@gmail.com>
2025-10-16 11:40:58 +0800

    test(oma-refresh): use mutex lock to fix tests (#592)

--- oma-refresh/src/lib.rs
+++ oma-refresh/src/lib.rs
@@ -3,3 +3,9 @@ pub mod db;
 pub mod inrelease;
 mod sourceslist;
 mod util;
+
+#[cfg(test)]
+mod test {
+    use std::sync::{LazyLock, Mutex};
+    pub(crate) static TEST_LOCK: LazyLock<Mutex<()>> = LazyLock::new(|| Mutex::new(()));
+}
