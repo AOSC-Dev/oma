ef28d45196a1ac3699272260d8764b795bbdb53e
eatradish <sakiiily@aosc.io>
2025-11-11 15:24:28 +0800

    refactor: improve `pkexec` handle local package path

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,7 @@
 use std::{
     env,
     io::ErrorKind,
-    path::{Path, PathBuf},
+    path::PathBuf,
     process::{Command, exit},
     sync::atomic::Ordering,
 };
@@ -45,23 +45,17 @@ pub fn root() -> Result<()> {
         return Ok(());
     }
 
-    let mut args = std::env::args().collect::<Vec<_>>();
-
     // 检测是否有 DISPLAY，如果有，则在提权时使用 pkexec
     // 通常情况下 SSH 连接不会有 DISPLAY 环境变量，除非开启 X11 Forwarding
     if (env::var("DISPLAY").is_ok() || env::var("WAYLAND_DISPLAY").is_ok()) && !is_wsl() {
-        // Workaround: 使用 pkexec 执行其它程序时，若你指定了相对路径
-        // pkexec 并不会以当前路径作为起点寻求这个位置
-        // 所以需要转换成绝对路径，再喂给 pkexec
-        file_path_canonicalize(&mut args);
-
         info!("{}", fl!("pkexec-tips-1"));
         info!("{}", fl!("pkexec-tips-2"));
 
         NOT_ALLOW_CTRLC.store(true, Ordering::Relaxed);
 
         let out = Command::new("pkexec")
-            .args(args)
+            .arg("--keep-cwd")
+            .args(std::env::args())
             .spawn()
             .and_then(|x| x.wait_with_output())
             .map_err(|e| anyhow!(fl!("execute-pkexec-fail", e = e.to_string())))?;
@@ -78,18 +72,6 @@ pub fn root() -> Result<()> {
 #[inline]
 pub fn is_root() -> bool {
     process::geteuid().is_root()
-}
-
-fn file_path_canonicalize(args: &mut Vec<String>) {
-    for arg in args {
-        if !arg.ends_with(".deb") {
-            continue;
-        }
-
-        let path = Path::new(&arg);
-        let path = path.canonicalize().unwrap_or(path.to_path_buf());
-        *arg = path.display().to_string();
-    }
 }
 
 #[inline]
