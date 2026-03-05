6f9fd1261e390827d2c440fcc1a787681c84cf53
eatradish <sakiiily@aosc.io>
2026-03-05 14:34:30 +0800

    refactor: refactor size_analyzer.rs to size_analyzer mod

---
+++ src/subcommand/size_analyzer/mod.rs
@@ -0,0 +1,215 @@
+mod key_binding;
+mod pkg;
+mod state;
+mod tui;
+
+use std::io::stdout;
+use std::time::Duration;
+
+use anyhow::anyhow;
+use clap::Args;
+use dialoguer::console::style;
+use oma_console::indicatif::HumanBytes;
+use oma_console::pager::{exit_tui, prepare_create_tui};
+use oma_pm::apt::{AptConfig, OmaApt, OmaAptArgs};
+use oma_pm::oma_apt::{Package, PackageSort};
+use oma_pm::pkginfo::OmaPackageWithoutVersion;
+use oma_utils::is_termux;
+
+use spdlog::info;
+use std::io::Write;
+use tabled::builder::Builder;
+use tabled::settings::{Alignment, Settings};
+
+use crate::config::OmaConfig;
+use crate::fl;
+use crate::subcommand::size_analyzer::pkg::PkgWrapper;
+use crate::subcommand::size_analyzer::tui::PkgSizeAnalyzer;
+use crate::subcommand::utils::{CommitChanges, auth_config};
+use crate::utils::{ExitHandle, dbus_check, is_root};
+use crate::{CliExecuter, error::OutputError};
+
+#[derive(Debug, Args)]
+pub struct SizeAnalyzer {
+    /// Only display packages size details
+    #[arg(short, long, help = fl!("clap-size-analyzer-details-help"))]
+    details: bool,
+    /// Resolve broken dependencies in the system
+    #[arg(short, long, help = fl!("clap-fix-broken-help"))]
+    fix_broken: bool,
+    /// Do not fix dpkg broken status
+    #[arg(short, long, help = fl!("clap-no-fix-dpkg-status-help"))]
+    no_fix_dpkg_status: bool,
+    /// Install package(s) without fsync(2)
+    #[arg(
+        long,
+        help = &**crate::args::FORCE_UNSAGE_IO_TRANSLATE
+    )]
+    force_unsafe_io: bool,
+    /// Ignore repository and package dependency issues
+    #[arg(long, help = fl!("clap-force-yes-help"))]
+    force_yes: bool,
+    /// Replace configuration file(s) in the system those shipped in the package(s) to be installed (invokes `dpkg --force-confnew`)
+    #[arg(long, help = fl!("clap-force-confnew-help"))]
+    force_confnew: bool,
+    /// Do not auto remove unnecessary package(s)
+    #[arg(long, help = fl!("clap-no-autoremove-help"))]
+    no_autoremove: bool,
+    /// Remove package(s) also remove configuration file(s), like apt purge
+    #[arg(long, visible_alias = "purge", help = fl!("clap-remove-config-help"))]
+    remove_config: bool,
+}
+
+impl CliExecuter for SizeAnalyzer {
+    fn execute(self, config: OmaConfig) -> Result<ExitHandle, OutputError> {
+        let SizeAnalyzer {
+            fix_broken,
+            no_fix_dpkg_status,
+            force_unsafe_io,
+            force_yes,
+            force_confnew,
+            no_autoremove,
+            remove_config,
+            details,
+        } = self;
+
+        let detail = if !is_root() && !is_termux() {
+            false
+        } else {
+            !details
+        };
+
+        let mut apt = OmaApt::new(
+            vec![],
+            OmaAptArgs::builder()
+                .another_apt_options(config.apt_options.clone())
+                .dpkg_force_unsafe_io(force_unsafe_io)
+                .force_yes(force_yes)
+                .dpkg_force_confnew(force_confnew)
+                .build(),
+            false,
+            AptConfig::new(),
+        )?;
+
+        let mut exit_code = ExitHandle::default();
+
+        if !detail {
+            let installed = installed_packages(&apt, true)
+                .unwrap()
+                .into_iter()
+                .map(|pkg| PkgWrapper { pkg })
+                .collect::<Vec<_>>();
+
+            let total_size = get_total_installed_size(&installed);
+
+            let res = installed.iter().map(|pkg| pkg.to_table_line(total_size));
+
+            let mut table = Builder::default();
+            res.for_each(|r| table.push_record([r.0, r.1, r.3]));
+
+            table.push_record([
+                style(HumanBytes(total_size)).green().to_string(),
+                "100%".to_string(),
+                fl!("psa-total"),
+            ]);
+
+            let table_settings = Settings::default().with(tabled::settings::style::Style::blank());
+
+            writeln!(
+                stdout(),
+                "{}",
+                table.build().with(table_settings).modify(
+                    tabled::settings::object::Columns::new(..1),
+                    Alignment::right()
+                )
+            )
+            .ok();
+            writeln!(stdout()).ok();
+            info!("{}", fl!("psa-without-root-tips"));
+        } else {
+            let _fds = dbus_check(false, &config)?;
+
+            let tui = PkgSizeAnalyzer::new(&apt);
+            let mut terminal =
+                prepare_create_tui().map_err(|e| anyhow!("Failed to create terminal: {e}"))?;
+
+            let remove_pkgs = tui
+                .run(&mut terminal, Duration::from_millis(250))
+                .map_err(|e| anyhow!("{e}"))?;
+
+            exit_tui(&mut terminal).map_err(|e| anyhow!("{e}"))?;
+
+            if remove_pkgs.is_empty() {
+                return Ok(exit_code);
+            }
+
+            apt.remove(
+                remove_pkgs
+                    .into_iter()
+                    .map(|p| OmaPackageWithoutVersion {
+                        raw_pkg: unsafe { p.pkg.unique() },
+                    })
+                    .collect::<Vec<_>>(),
+                remove_config,
+                no_autoremove,
+            )?;
+
+            let auth_config = auth_config(&config.sysroot);
+            let auth_config = auth_config.as_ref();
+
+            exit_code = CommitChanges::builder()
+                .apt(apt)
+                .dry_run(config.dry_run)
+                .no_fixbroken(!fix_broken)
+                .no_progress(config.no_progress())
+                .sysroot(config.sysroot.to_string_lossy().to_string())
+                .protect_essential(config.protect_essentials)
+                .yes(false)
+                .remove_config(remove_config)
+                .autoremove(!no_autoremove)
+                .network_thread(config.download_threads)
+                .maybe_auth_config(auth_config)
+                .fix_dpkg_status(!no_fix_dpkg_status)
+                .yn_mode(config.yn_mode)
+                .build()
+                .run()?;
+        }
+
+        Ok(exit_code)
+    }
+}
+
+pub(crate) fn installed_packages(
+    apt: &OmaApt,
+    small_to_big: bool,
+) -> Result<Vec<Package<'_>>, OutputError> {
+    let mut installed_packages = apt
+        .cache
+        .packages(&PackageSort::default().installed())
+        .collect::<Vec<_>>();
+
+    if small_to_big {
+        installed_packages.sort_unstable_by(|a, b| {
+            a.installed()
+                .unwrap()
+                .installed_size()
+                .cmp(&b.installed().unwrap().installed_size())
+        });
+    } else {
+        installed_packages.sort_unstable_by(|a, b| {
+            b.installed()
+                .unwrap()
+                .installed_size()
+                .cmp(&a.installed().unwrap().installed_size())
+        });
+    }
+
+    Ok(installed_packages)
+}
+
+fn get_total_installed_size(installed: &[PkgWrapper]) -> u64 {
+    installed
+        .iter()
+        .map(|p| p.pkg.installed().unwrap().installed_size())
+        .sum()
+}
---
+++ src/subcommand/size_analyzer/tui.rs
@@ -0,0 +1,275 @@
+use std::{
+    io,
+    ops::ControlFlow,
+    rc::Rc,
+    time::{Duration, Instant},
+};
+
+use dialoguer::console;
+use oma_pm::apt::OmaApt;
+use ratatui::{
+    Frame, Terminal,
+    crossterm::event::{self},
+    layout::{Constraint, Direction, Flex, Layout, Rect},
+    prelude::Backend,
+    style::{Color, Style},
+    text::{Line, Span, Text},
+    widgets::{
+        Block, Borders, Clear, List, Paragraph, Scrollbar, ScrollbarOrientation, ScrollbarState,
+    },
+};
+
+use crate::{
+    WRITER, fl,
+    subcommand::size_analyzer::{
+        get_total_installed_size, installed_packages,
+        pkg::PkgWrapper,
+        state::{StatefulList, Window},
+    },
+};
+
+pub(crate) struct PkgSizeAnalyzer<'a> {
+    pub(crate) apt: &'a OmaApt,
+    pub(crate) remove_package: StatefulList<PkgWrapper<'a>>,
+    pub(crate) installed: StatefulList<PkgWrapper<'a>>,
+    pub(crate) window: Window,
+    pub(crate) total_installed_size: u64,
+    pub(crate) popup: Option<String>,
+    pub(crate) installed_scroll_state: ScrollbarState,
+    pub(crate) installed_scroll: usize,
+}
+
+impl<'a> PkgSizeAnalyzer<'a> {
+    pub(crate) const PAGE_LEN: usize = 10;
+
+    pub(crate) fn new(apt: &'a OmaApt) -> Self {
+        Self {
+            apt,
+            remove_package: StatefulList::with_items(vec![]),
+            installed: StatefulList::with_items(vec![]),
+            window: Window::Installed,
+            total_installed_size: 0,
+            popup: None,
+            installed_scroll_state: ScrollbarState::new(0),
+            installed_scroll: 0,
+        }
+    }
+
+    pub(crate) fn run<B: Backend>(
+        mut self,
+        terminal: &mut Terminal<B>,
+        tick_rate: Duration,
+    ) -> io::Result<Vec<PkgWrapper<'a>>> {
+        let mut last_tick = Instant::now();
+        self.installed = StatefulList::with_items(
+            installed_packages(self.apt, false)
+                .unwrap()
+                .into_iter()
+                .map(|p| PkgWrapper { pkg: p })
+                .collect::<Vec<_>>(),
+        );
+
+        self.installed_scroll_state = self
+            .installed_scroll_state
+            .content_length(self.installed.items.len());
+        self.total_installed_size = get_total_installed_size(&self.installed.items);
+        self.installed.state.select_first();
+
+        loop {
+            terminal
+                .draw(|f| self.ui(f))
+                .map_err(|e| io::Error::other(e.to_string()))?;
+
+            if event::poll(tick_rate)? {
+                let event::Event::Key(key) = event::read()? else {
+                    continue;
+                };
+
+                match self.handle_key_event(key) {
+                    ControlFlow::Continue(()) => continue,
+                    ControlFlow::Break(pkgs) => return Ok(pkgs),
+                }
+            }
+
+            if last_tick.elapsed() >= tick_rate {
+                last_tick = Instant::now();
+            }
+        }
+    }
+
+    fn ui(&mut self, f: &mut Frame) {
+        let main_layout = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([
+                Constraint::Length(1), // header
+                Constraint::Min(0),    // packages
+                Constraint::Length(1), // tips
+            ])
+            .split(f.area());
+
+        f.render_widget(
+            Block::default()
+                .title(format!(
+                    " {} v{} - {}",
+                    fl!("oma"),
+                    env!("CARGO_PKG_VERSION"),
+                    fl!("packages-size-analyzer"),
+                ))
+                .style(if self.remove_package.items.is_empty() {
+                    Style::default().bg(Color::White).fg(Color::Black)
+                } else {
+                    Style::default().bg(Color::Yellow).fg(Color::Black)
+                }),
+            main_layout[0],
+        );
+
+        let chunks = Layout::default()
+            .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+            .direction(Direction::Horizontal)
+            .split(main_layout[1]);
+
+        let area = if self.remove_package.items.is_empty() {
+            main_layout[1]
+        } else {
+            chunks[0]
+        };
+
+        f.render_stateful_widget(
+            List::new(self.installed.items.iter().map(|p| {
+                p.to_installed_line(
+                    self.total_installed_size,
+                    self.remove_package.items.contains(p),
+                )
+            }))
+            .block(
+                Block::default()
+                    .title(fl!(
+                        "psa-installed-window",
+                        count = self.installed.items.len(),
+                        size = oma_utils::human_bytes::HumanBytes(self.total_installed_size)
+                            .to_string()
+                    ))
+                    .borders(Borders::ALL)
+                    .style(highlight_window(self.window, Window::Installed)),
+            )
+            .highlight_style(Style::default().bg(Color::Rgb(59, 64, 70))),
+            area,
+            &mut self.installed.state,
+        );
+
+        f.render_stateful_widget(
+            Scrollbar::new(ScrollbarOrientation::VerticalRight)
+                .begin_symbol(Some("↑"))
+                .end_symbol(Some("↓")),
+            area,
+            &mut self.installed_scroll_state,
+        );
+
+        if !self.remove_package.items.is_empty() {
+            f.render_stateful_widget(
+                List::new(
+                    self.remove_package
+                        .items
+                        .iter()
+                        .map(|p| p.to_remove_line(chunks[1].width - 2)),
+                )
+                .highlight_style(Style::default().bg(Color::Rgb(59, 64, 70)))
+                .block(
+                    Block::new()
+                        .title(fl!(
+                            "psa-remove-window",
+                            count = self.remove_package.items.len(),
+                            size = oma_utils::human_bytes::HumanBytes(
+                                self.remove_package
+                                    .items
+                                    .iter()
+                                    .map(|p| p.pkg.installed().unwrap().installed_size())
+                                    .sum::<u64>()
+                            )
+                            .to_string()
+                        ))
+                        .borders(Borders::ALL)
+                        .style(highlight_window(self.window, Window::RemovePending)),
+                ),
+                chunks[1],
+                &mut self.remove_package.state,
+            );
+        }
+
+        render_tips(f, &main_layout);
+
+        if let Some(popup) = &self.popup {
+            let block = Block::bordered();
+            let area = popup_area(
+                main_layout[1],
+                console::measure_text_width(popup) as u16 + 10,
+                6,
+            );
+            let inner = block.inner(area);
+            f.render_widget(Clear, area); //this clears out the background
+            f.render_widget(block, area);
+            f.render_widget(
+                Text::from(vec![
+                    Line::raw(popup),
+                    Line::raw(""),
+                    Line::raw(fl!("tui-continue-tips")),
+                ]),
+                inner,
+            );
+        }
+    }
+}
+
+fn render_tips(f: &mut Frame<'_>, main_layout: &Rc<[Rect]>) {
+    match WRITER.get_length() {
+        0..=62 => {}
+        63..=153 => {
+            f.render_widget(
+                Paragraph::new(Line::from(vec![
+                    Span::raw("Quicknav: "),
+                    Span::styled("TAB", Style::new().blue()),
+                    Span::raw(" / "),
+                    Span::styled("ESC", Style::new().blue()),
+                    Span::raw(" / "),
+                    Span::styled("Ctrl+A", Style::new().blue()),
+                    Span::raw(" / "),
+                    Span::styled("Ctrl+C", Style::new().blue()),
+                ])),
+                main_layout[2],
+            );
+        }
+        154.. => {
+            f.render_widget(
+                Paragraph::new(Line::from(vec![
+                    Span::styled("TAB", Style::new().blue()),
+                    Span::raw(format!(" => {}, ", fl!("tui-start-2"))),
+                    Span::styled("ESC", Style::new().blue()),
+                    Span::raw(format!(" => {}, ", fl!("tui-start-4"))),
+                    Span::styled("Ctrl+A", Style::new().blue()),
+                    Span::raw(format!(" => {}, ", fl!("psa-autoremove"))),
+                    Span::styled("Ctrl+C", Style::new().blue()),
+                    Span::raw(format!(" => {}", fl!("tui-start-7"))),
+                ])),
+                main_layout[2],
+            );
+        }
+    }
+}
+
+fn highlight_window(current: Window, needs: Window) -> Style {
+    if current == needs {
+        Style::default()
+    } else {
+        Style::default().fg(Color::DarkGray)
+    }
+}
+
+/// helper function to create a centered rect using up certain percentage of the available rect `r`
+fn popup_area(area: Rect, x: u16, y: u16) -> Rect {
+    let vertical = Layout::vertical([Constraint::Length(y)]).flex(Flex::Center);
+    let horizontal = Layout::horizontal([Constraint::Length(x)]).flex(Flex::Center);
+    let [area] = vertical.areas(area);
+    let [area] = horizontal.areas(area);
+
+    area
+}
