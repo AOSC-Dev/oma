5064217735013c413432b690f2843c9fe829cc2d
eatradish <sakiiily@aosc.io>
2026-06-15 12:51:32 +0800

    chore(oma-refresh): refactor example

--- oma-refresh/examples/update.rs
+++ oma-refresh/examples/update.rs
@@ -4,13 +4,12 @@ use oma_fetch::reqwest::ClientBuilder;
 use oma_refresh::db::{Event, OmaRefresh, RefreshError};
 use oma_utils::dpkg::dpkg_arch;
 
-#[tokio::main]
-async fn main() -> Result<(), RefreshError> {
+fn main() -> Result<(), RefreshError> {
     rustls::crypto::ring::default_provider()
         .install_default()
         .unwrap();
     let p = Path::new("./oma-fetcher-test");
-    tokio::fs::create_dir_all(p).await.unwrap();
+    std::fs::create_dir_all(p).unwrap();
     let client = ClientBuilder::new().user_agent("oma").build().unwrap();
 
     let (tx, rx) = flume::unbounded();
@@ -24,8 +23,8 @@ async fn main() -> Result<(), RefreshError> {
         .refresh_topics(false)
         .build();
 
-    tokio::spawn(async move {
-        while let Ok(event) = rx.recv_async().await {
+    std::thread::spawn(move || {
+        while let Ok(event) = rx.recv() {
             println!("{:#?}", event);
             if let Event::Done = event {
                 break;
